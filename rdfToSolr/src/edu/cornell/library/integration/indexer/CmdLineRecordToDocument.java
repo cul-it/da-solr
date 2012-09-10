1605ff92ab409dcecb222a4929efcab27f4b3c66
Brian Caruso <bdc34@cornell.edu>
2012-09-10 12:16:55 -0400

    Changing how CmdLineRecordToDocument

--- rdfToSolr/src/edu/cornell/library/integration/indexer/CmdLineRecordToDocument.java
+++ rdfToSolr/src/edu/cornell/library/integration/indexer/CmdLineRecordToDocument.java
@@ -1,5 +1,7 @@
 package edu.cornell.library.integration.indexer;
 
+import java.lang.reflect.Constructor;
+
 import org.apache.solr.common.SolrInputDocument;
 
 import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFService;
@@ -21,32 +23,43 @@ public class CmdLineRecordToDocument {
 		
 		String readEndpointURI = args[0];
 		String recordURI = args[1];
+		String recToDocImplClassName = args[2];
+		
+		//make an instance of the record 2 document class
+		RecordToDocument r2d = getRecordToDocumentImpl( recToDocImplClassName );		
 		
 		//setup SPARQL RDFService
 		RDFService queryService = new RDFServiceSparql( readEndpointURI );
-	
-		SolrInputDocument doc = null;
-		Exception error = null;		
-		try{
-			//call RecordToDocument
-			doc = (new RecordToDocumentImpl()).buildDoc(recordURI, queryService);
+					
+		try{						
+			SolrInputDocument doc = r2d.buildDoc(recordURI, queryService);
+			System.out.print(doc.toString());
 		}catch(Exception ex){
-			error = ex;
+			System.err.print( ex.toString());
+			ex.printStackTrace(System.err);
+			System.exit(1);
 		}finally{
 			queryService.close();
-		}
-				
-		//output solr document
-		if( doc != null)
-			System.out.print(doc.toString());
-		if( error != null ){
-			System.err.print( error.toString());
-			error.printStackTrace(System.err);
-		}
+		}			
 	}
+	
+	private RecordToDocument getRecordToDocumentImpl( String recToDocImplClassName){
+		try{
+			Class recToDocImplClass = Class.forName(recToDocImplClassName);
+			Constructor zeroArgCons = recToDocImplClass.getConstructor(null);
+			return (RecordToDocument) zeroArgCons.newInstance(null);			
+		}catch(Exception ex){
+			System.err.println("could not instanciate class " + recToDocImplClassName);
+			System.err.print( ex.toString());
+			ex.printStackTrace(System.err);
+			System.exit(1);
+		}	
+		return null;
+	}
+	
 
 	private boolean help(String[] args) {		
-		if( args == null || args.length != 2 ||
+		if( args == null || args.length != 3 ||
 		    (args.length == 1 && args[0] != null && args[0].toLowerCase().startsWith("-h")) ){
 			System.out.print(helptext);
 			return true;
@@ -55,5 +68,5 @@ public class CmdLineRecordToDocument {
 		}				
 	}
 	
-	private static final String helptext ="Expected: URLofSPARQLEndpoint URLofRecordToIndex\n";
+	private static final String helptext ="Expected: URLofSPARQLEndpoint URLofRecordToIndex FQPN_of_RecordToDocument\n";
 }
