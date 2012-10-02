d6b6ac7695ba0c2a53f10893de781c551582db6f
Frances Webb <frances.webb@cornell.edu>
2012-10-02 16:44:39 -0400

    fix callno query bug

--- rdfToSolr/src/edu/cornell/library/integration/indexer/RecordToDocumentMARC.java
+++ rdfToSolr/src/edu/cornell/library/integration/indexer/RecordToDocumentMARC.java
@@ -158,12 +158,12 @@ public class RecordToDocumentMARC extends RecordToDocumentBase {
 			        	"  ?hold marcrdf:hasField ?hold852.\n" +
 			        	"  ?hold852 marcrdf:tag \"852\".\n" +
 			        	"  ?hold852 marcrdf:hasSubfield ?hold852h.\n" +
-			        	"  ?hold852b marcrdf:code \"h\".\n" +
-			        	"  ?hold852b marcrdf:value ?part1.\n" +
+			        	"  ?hold852h marcrdf:code \"h\".\n" +
+			        	"  ?hold852h marcrdf:value ?part1.\n" +
 			        	"  OPTIONAL {\n" +
 			        	"    ?hold852 marcrdf:hasSubfield ?hold852i.\n" +
-			        	"    ?hold852b marcrdf:code \"i\".\n" +
-			        	"    ?hold852b marcrdf:value ?part2. }\n" +
+			        	"    ?hold852i marcrdf:code \"i\".\n" +
+			        	"    ?hold852i marcrdf:value ?part2. }\n" +
 			        	"}").
 				    addMainStoreQuery("bib_callno",
 					    "SELECT ?part1 ?part2 \n"+
