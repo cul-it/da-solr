127c2131daa22b2fbfc0397c70c4cfac852e0e22
Brian Caruso <bdc34@cornell.edu>
2012-10-11 14:59:21 -0400

    Adding debugging ResultSetToFields but it doesn't work well

---
+++ rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/resultSetToFields/DebuggingResultSetToFields.java
@@ -0,0 +1,70 @@
+package edu.cornell.library.integration.indexer.resultSetToFields;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.solr.common.SolrInputField;
+
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+
+/**
+ * Print result set to STDOUT and execute inner ResultSetToFields
+ *
+ */
+public class DebuggingResultSetToFields implements ResultSetToFields {
+	
+	ResultSetToFields innerRstf;
+	
+	public DebuggingResultSetToFields(ResultSetToFields innerRstf) {
+		super();
+		this.innerRstf = innerRstf;
+	}
+
+	@Override
+	public Map<? extends String, ? extends SolrInputField> toFields(
+			Map<String, ResultSet> results) throws Exception {
+		System.out.print( makeDebugStr(results) );
+		return innerRstf.toFields(results);
+	}
+
+	String makeDebugStr(Map<String, ResultSet> results) {
+		if( results == null )
+			return "The Map of ResultSets was Null!";
+		String out ="";
+		for( String key : results.keySet()){
+			out = out + "ResultSet " + key + "\n";
+			out = out + resultsToStr( results.get(key));
+		}
+		return out;
+	}
+
+	private String resultsToStr(ResultSet resultSet) {
+		if( resultSet == null ){
+			return "    null";
+		}
+		String out ="";
+		while( resultSet.hasNext()){
+			QuerySolution qs = resultSet.nextSolution();			
+			out = out + qsToStr(qs) ;
+		}
+		return out;
+	}
+
+	private String qsToStr(QuerySolution qs) {
+		if(qs == null )
+			return  "    null QuerySolution\n";
+		
+		String out = "";
+		Iterator<String> it = qs.varNames();
+		while(it.hasNext()){
+			String varName = it.next();
+			out = out 
+					+ "    " + varName + ": '" 
+					+ ResultSetUtilities.nodeToString( qs.get(varName) ) 
+					+ "'";
+		}
+		return out + "\n";
+	}
+
+}
