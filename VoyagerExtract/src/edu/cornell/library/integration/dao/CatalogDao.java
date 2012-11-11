9a40be03cfc368d8241925a34d200189915660d1
John Fereira <jaf30@cornell.edu>
2012-11-11 12:58:09 -0500

    add methods for retrieving location data

--- VoyagerExtract/src/edu/cornell/library/integration/dao/CatalogDao.java
+++ VoyagerExtract/src/edu/cornell/library/integration/dao/CatalogDao.java
@@ -3,7 +3,11 @@ package edu.cornell.library.integration.dao;
 import java.util.List;
 import java.util.Map;
 
+import edu.cornell.library.integration.bo.Location;
+
 
 public interface CatalogDao {
    public int sanityCheck();
+   
+   public List<Location> getAllLocation() throws Exception;
 }
