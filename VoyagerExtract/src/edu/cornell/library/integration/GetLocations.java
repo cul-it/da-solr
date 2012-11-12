015266067029dc8e573d8c4ad12cfdcd7d860b80
John Fereira <jaf30@cornell.edu>
2012-11-12 12:35:05 -0500

    refactor loading of beans into run method

--- VoyagerExtract/src/edu/cornell/library/integration/GetLocations.java
+++ VoyagerExtract/src/edu/cornell/library/integration/GetLocations.java
@@ -1,7 +1,9 @@
 package edu.cornell.library.integration;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +12,9 @@ import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory; 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.thoughtworks.xstream.XStream;
@@ -17,28 +22,105 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import edu.cornell.library.integration.bo.Location;
 import edu.cornell.library.integration.bo.LocationInfo;
+import edu.cornell.library.integration.config.IntegrationDataProperties;
 import edu.cornell.library.integration.service.CatalogService;
 import edu.cornell.library.integration.service.DavService;
 import edu.cornell.library.integration.util.ObjectUtils; 
 
 public class GetLocations {
    
-   
    /** Logger for this class and subclasses */
    protected final Log logger = LogFactory.getLog(getClass()); 
 
+   private DavService davService;
+   private CatalogService catalogService;
+   private IntegrationDataProperties integrationDataProperties;
+   
+
+   /**
+    * default constructor
+    */
    public GetLocations() { 
        
-   }   
- 
+   }  
    
-   public static void main(String[] args) {
-      ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-      CatalogService catalogService = (CatalogService) ctx.getBean("catalogService");
-      DavService davService = (DavService) ctx.getBean("davService");
+      
+   /**
+    * @return the davService
+    */
+   public DavService getDavService() {
+      return this.davService;
+   }
+
+   /**
+    * @param davService the davService to set
+    */
+   public void setDavService(DavService davService) {
+      this.davService = davService;
+   }
+
+   /**
+    * @return the catalogService
+    */
+   public CatalogService getCatalogService() {
+      return this.catalogService;
+   }
+
+   /**
+    * @param catalogService the catalogService to set
+    */
+   public void setCatalogService(CatalogService catalogService) {
+      this.catalogService = catalogService;
+   }
+
+   
+   /**
+    * @return the integrationDataProperties
+    */
+   public IntegrationDataProperties getIntegrationDataProperties() {
+      return this.integrationDataProperties;
+   }
+
+
+   /**
+    * @param integrationDataProperties the integrationDataProperties to set
+    */
+   public void setIntegrationDataProperties(
+         IntegrationDataProperties integrationDataProperties) {
+      this.integrationDataProperties = integrationDataProperties;
+   } 
+
+   /**
+    * 
+    */
+   public void run() {
+      ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+     
+      if (ctx.containsBean("catalogService")) {
+         setCatalogService((CatalogService) ctx.getBean("catalogService"));
+      } else {
+         System.err.println("Could not get catalogService");
+         System.exit(-1);
+      }
+
+      if (ctx.containsBean("davService")) {
+         setDavService((DavService) ctx.getBean("davService"));
+      } else {
+         System.err.println("Could not get davService");
+         System.exit(-1);
+      }
+
+      if (ctx.containsBean("integrationDataProperties")) {
+         setIntegrationDataProperties((IntegrationDataProperties) ctx.getBean("integrationDataProperties"));
+      } else {
+         System.err.println("Could not get integrationDataProperties");
+         System.exit(-1);
+      }    
+
+       
       List<Location> locationList = new ArrayList<Location>();
       try {
-         locationList = catalogService.getAllLocation();
+         locationList = getCatalogService().getAllLocation();
       } catch (Exception e) {
          // TODO Auto-generated catch block
          e.printStackTrace();
@@ -49,11 +131,30 @@ public class GetLocations {
       LocationInfo locationInfo = new LocationInfo();
       locationInfo.setLocationList(locationList);
       String xml = getXml(locationInfo);
-      saveXml(xml);
+      
+      try {
+         saveXml(xml);
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }   
       
    }
+
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+     GetLocations app = new GetLocations();
+     app.run();
+   }
    
-   public static String getXml(LocationInfo locationInfo) {
+   /**
+    * @param locationInfo
+    * @return
+    */
+   public String getXml(LocationInfo locationInfo) {
       // use xstream to rend the model into xml
       XStream xstream = new XStream(new DomDriver()); // does not require XPP3 library
       xstream.alias("location", Location.class);
@@ -65,14 +166,29 @@ public class GetLocations {
       return xml;
    }
    
-   public static void saveXml(String xml) {
+   /**
+    * @param xml
+    * @throws Exception
+    */
+   public void saveXml(String xml) throws Exception {
+      try {
+         byte[] bytes = xml.getBytes("UTF-8");
+         InputStream isr = new  ByteArrayInputStream(bytes);
+         
+         String url = getIntegrationDataProperties().getVoyagerLocationsXml() + "/locations.xml";      
+         getDavService().saveFile(url, isr);
+      } catch (Exception ex) {
+         throw ex;
+      }
+   }
+   
+   public void saveXmlToFile(String xml) throws Exception {
       String fname = "/usr/local/src/integrationlayer/VoyagerExtract/locations.xml";
       File file = new File(fname);
       try {
          FileUtils.writeStringToFile(file, xml);
-      } catch (IOException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
+      } catch (IOException ex) {
+         throw ex;
       }
 
    }
