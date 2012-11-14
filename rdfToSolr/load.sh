84fca0e76184f68bbb12f86ec0f3f243f3ff139a
Brian Caruso <bdc34@cornell.edu>
2012-11-14 14:26:04 -0500

    change to load.sh

--- rdfToSolr/load.sh
+++ rdfToSolr/load.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 SPARQLURL=http://bdc34-dev.library.cornell.edu:8890/sparql 
-
 BATCHSIZE=10000
 
+# get the total number of bib records
 COUNTQUERY='select count(distinct(?uri)) { ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://marcrdf.library.cornell.edu/canonical/0.1/BibliographicRecord> . }'
+TOTALBIBS=$( curl -g --data-urlencode "query=$COUNTQUERY" $SPARQLURL | xpath  -e "/sparql/results/result/binding/literal/text()" )
 
-#TOTALBIBS=$( curl -g --data-urlencode "query=$COUNTQUERY" $SPARQLURL )
-TOTALBIBS=2789030
+WORKDIR="./tmpWork"
+mkdir $WORKDIR
 
-echo "total number or bib records to load: $TOTALBIBS"
-echo $BASH_VERSION
+# make a script to load each of the files
 for (( OFFSET=0; OFFSET<$TOTALBIBS ;OFFSET=$OFFSET+$BATCHSIZE ))
 do
+  BATCHNAME=batch.$OFFSET.sh
   BIBURIQUERY="select ?uri { ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://marcrdf.library.cornell.edu/canonical/0.1/BibliographicRecord> . } LIMIT $BATCHSIZE OFFSET $OFFSET"
-  curl -g --data-urlencode \""query=$BIBURIQUERY"\" $SPARQLURL | ./stdinRecordToDocument.sh
+  
+  echo "curl -g --data-urlencode \""query=$BIBURIQUERY"\" $SPARQLURL | grep binding | sed \"s/^.*\(http[^<]*\).*$/\1/g\" | ./stdinRecordToDocument.sh  && rm $WORKDIR/$BATCHNAME " > $WORKDIR/$BATCHNAME
 done
