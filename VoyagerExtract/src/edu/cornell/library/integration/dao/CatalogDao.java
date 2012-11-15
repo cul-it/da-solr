58fa65ea8ecb213133c039946fc042994b397fc2
John Fereira <jaf30@cornell.edu>
2012-11-15 11:30:23 -0500

    work on pulling Bib data from database using Clob

--- VoyagerExtract/src/edu/cornell/library/integration/dao/CatalogDao.java
+++ VoyagerExtract/src/edu/cornell/library/integration/dao/CatalogDao.java
@@ -3,6 +3,7 @@ package edu.cornell.library.integration.dao;
 import java.util.List;
 import java.util.Map;
 
+import edu.cornell.library.integration.bo.BibData;
 import edu.cornell.library.integration.bo.Location;
 
 
@@ -10,4 +11,9 @@ public interface CatalogDao {
    public int sanityCheck();
    
    public List<Location> getAllLocation() throws Exception;
+   
+   public List<String> getRecentBibIds() throws Exception;
+   
+   public BibData getBibData(String bibid) throws Exception;
+   
 }
--- VoyagerExtract/src/edu/cornell/library/integration/dao/CatalogDaoImpl.java
+++ VoyagerExtract/src/edu/cornell/library/integration/dao/CatalogDaoImpl.java
@@ -1,15 +1,26 @@
 package edu.cornell.library.integration.dao;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringWriter;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.Clob;
+
+import oracle.jdbc.OracleResultSet;
+import oracle.sql.CLOB;
+import java.text.SimpleDateFormat; 
+import java.util.Calendar; 
+import java.util.List; 
 
 import javax.sql.DataSource;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -20,6 +31,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
  
+import edu.cornell.library.integration.bo.BibData;
 import edu.cornell.library.integration.bo.Location;
 import edu.cornell.library.integration.dao.CatalogDao; 
 
@@ -127,6 +139,51 @@ public class CatalogDaoImpl implements CatalogDao {
 
       
    }
+   
+   public List<String> getRecentBibIds() throws Exception {
+       
+      SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      Calendar now = Calendar.getInstance();
+      Calendar earlier = now;
+      earlier.add(Calendar.HOUR, -3);
+      String ds = df.format(earlier.getTime());
+      
+      String sql = "" 
+            +" SELECT BIB_ID FROM BIB_HISTORY"
+            +" WHERE to_char(BIB_HISTORY.ACTION_DATE, 'yyyy-MM-dd HH:mm:ss') > '" + ds + "'"   
+            +" AND SUPPRESS_IN_OPAC = 'N'";
+      try {
+         List<String> bibIdList =  this.jdbcTemplate.query(sql, new StringMapper());
+         return bibIdList;
+      } catch (EmptyResultDataAccessException ex) {
+         logger.warn("Empty result set");
+         logger.info("Query was: "+sql);
+         return null;
+      } catch (Exception ex) {
+         logger.error("Exception: ", ex);
+         logger.info("Query was: "+sql);
+         throw ex;
+      } 
+   }
+   
+   public BibData getBibData(String bibid) throws Exception {
+       
+      String sql = ""
+         +"SELECT BIB_ID, MARC_RECORD FROM CORNELLDB.BIBBLOB_VW WHERE BIB_ID = '"+ bibid +"'";
+      try {
+         BibData bibData =  (BibData) this.jdbcTemplate.queryForObject(sql, new BibDataMapper());
+         return bibData;
+      } catch (EmptyResultDataAccessException ex) {
+         logger.warn("Empty result set");
+         logger.info("Query was: "+sql);
+         return null;
+      } catch (Exception ex) {
+         logger.error("Exception: ", ex);
+         logger.info("Query was: "+sql);
+         throw ex;
+      }  
+      
+   }
 
 
 
@@ -147,6 +204,32 @@ public class CatalogDaoImpl implements CatalogDao {
 
    }
    
+   /**
+    * @param clob
+    * @return
+    * @throws Exception
+    */
+   protected static String convertClobToString(Clob clob)  { 
+        
+      try {        
+         System.out.println("clob length: "+ clob.length());
+         char clobVal[] = new char[(int) clob.length()];         
+         Reader reader = clob.getCharacterStream();
+         reader.read(clobVal);
+         StringWriter sw = new StringWriter();
+         sw.write(clobVal);
+         StringBuffer sb = sw.getBuffer();
+         return sb.toString();            
+         
+      } catch(Exception e) { 
+         e.printStackTrace();
+         return "";
+      }   
+      
+   }
+   
+   
+   
    private static final class LocationMapper implements RowMapper {
       public Location mapRow(ResultSet rs, int rowNum) throws SQLException {
          Location location = new Location(); 
@@ -163,6 +246,33 @@ public class CatalogDaoImpl implements CatalogDao {
         // location.setLibraryDisplayName(rs.getString("LIBRARY_DISPLAY_NAME"));
          return location;
        }
+   }
+   
+   /**
+    * @author jaf30
+    *
+    */
+   private static final class BibDataMapper implements RowMapper {
+      public BibData mapRow(ResultSet rs, int rowNum) throws  SQLException {
+         BibData bibData = new BibData(); 
+         bibData.setBibId(rs.getString("BIB_ID")); 
+         Clob clob =   rs.getClob("MARC_RECORD");
+         bibData.setClob(clob);
+         bibData.setRecord(convertClobToString(clob));
+         return bibData;
+       }
+   }
+   
+   /**
+    * @author jaf30
+    *
+    */
+   private static final class StringMapper implements RowMapper {
+      public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+         String s = new String();
+         s = rs.getString(1);
+         return s;
+      }
    }
 
 }
