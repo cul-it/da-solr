88770519940c36bba8fd8c504c50e74c5b1d3e1f
John Fereira <jaf30@cornell.edu>
2012-11-15 11:27:57 -0500

    initial versions

---
+++ VoyagerExtract/src/edu/cornell/library/integration/GetRecentBibData.java
@@ -0,0 +1,178 @@
+package edu.cornell.library.integration;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Clob;
+import java.util.ArrayList;
+import java.util.List;
+
+ 
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory; 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import edu.cornell.library.integration.bo.BibData;
+import edu.cornell.library.integration.bo.Location;
+import edu.cornell.library.integration.bo.LocationInfo;
+import edu.cornell.library.integration.config.IntegrationDataProperties;
+import edu.cornell.library.integration.service.CatalogService;
+import edu.cornell.library.integration.service.DavService;
+import edu.cornell.library.integration.util.ObjectUtils; 
+
+public class GetRecentBibData {
+   
+   /** Logger for this class and subclasses */
+   protected final Log logger = LogFactory.getLog(getClass()); 
+
+   private DavService davService;
+   private CatalogService catalogService;
+   private IntegrationDataProperties integrationDataProperties;
+   
+
+   /**
+    * default constructor
+    */
+   public GetRecentBibData() { 
+       
+   }  
+   
+      
+   /**
+    * @return the davService
+    */
+   public DavService getDavService() {
+      return this.davService;
+   }
+
+   /**
+    * @param davService the davService to set
+    */
+   public void setDavService(DavService davService) {
+      this.davService = davService;
+   }
+
+   /**
+    * @return the catalogService
+    */
+   public CatalogService getCatalogService() {
+      return this.catalogService;
+   }
+
+   /**
+    * @param catalogService the catalogService to set
+    */
+   public void setCatalogService(CatalogService catalogService) {
+      this.catalogService = catalogService;
+   }
+
+   
+   /**
+    * @return the integrationDataProperties
+    */
+   public IntegrationDataProperties getIntegrationDataProperties() {
+      return this.integrationDataProperties;
+   }
+
+
+   /**
+    * @param integrationDataProperties the integrationDataProperties to set
+    */
+   public void setIntegrationDataProperties(
+         IntegrationDataProperties integrationDataProperties) {
+      this.integrationDataProperties = integrationDataProperties;
+   } 
+   
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+     GetRecentBibData app = new GetRecentBibData();
+     if (args.length != 1 ) {
+        System.err.println("You must provide a destination dir as an argument");
+        System.exit(-1);
+     }
+     String destDir = args[0];
+     app.run(destDir);
+   }
+
+   /**
+    * 
+    */
+   public void run(String destDir) {
+      System.out.println("Get Recent BibData");
+      ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+     
+      if (ctx.containsBean("catalogService")) {
+         setCatalogService((CatalogService) ctx.getBean("catalogService"));
+      } else {
+         System.err.println("Could not get catalogService");
+         System.exit(-1);
+      }
+
+      if (ctx.containsBean("davService")) {
+         setDavService((DavService) ctx.getBean("davService"));
+      } else {
+         System.err.println("Could not get davService");
+         System.exit(-1);
+      }
+
+     /* if (ctx.containsBean("integrationDataProperties")) {
+         setIntegrationDataProperties((IntegrationDataProperties) ctx.getBean("integrationDataProperties"));
+      } else {
+         System.err.println("Could not get integrationDataProperties");
+         System.exit(-1);
+      }  */  
+
+       
+      List<String> bibIdList = new ArrayList<String>();
+      try {
+         System.out.println("Getting recent bibids");
+         bibIdList = getCatalogService().getRecentBibIds();
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
+      for (String bibid: bibIdList) {
+         try {            
+            System.out.println("Getting bibRecord for bibid: "+bibid);
+            BibData bibData = catalogService.getBibData(bibid);            
+            saveBibData(bibData, destDir);
+         } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }   
+            
+      }
+      
+   }
+
+
+   /**
+    * @param xml
+    * @throws Exception
+    */
+   public void saveBibData(BibData bibData, String destDir) throws Exception {
+      try {
+         String bibid = bibData.getBibId();
+         String record = bibData.getRecord();     
+         
+         InputStream isr = new ByteArrayInputStream(record.getBytes("UTF-8"));
+         
+         String url = destDir + "/" + bibid +".mrc";      
+         getDavService().saveFile(url, isr);
+      } catch (Exception ex) {
+         throw ex;
+      }
+   } 
+}
