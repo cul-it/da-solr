68078adde32aa541a2818d0fa579815a5ed9a60b
John Fereira <jaf30@cornell.edu>
2012-12-03 08:46:33 -0500

    add getAllLocation getBibBlob methods

--- VoyagerExtract/src/edu/cornell/library/integration/dao/CatalogDao.java
+++ VoyagerExtract/src/edu/cornell/library/integration/dao/CatalogDao.java
@@ -3,17 +3,48 @@ package edu.cornell.library.integration.dao;
 import java.util.List;
 import java.util.Map;
 
+import edu.cornell.library.integration.bo.BibBlob;
 import edu.cornell.library.integration.bo.BibData;
 import edu.cornell.library.integration.bo.Location;
 
 
 public interface CatalogDao {
+   
+   /**
+    * @return
+    */
    public int sanityCheck();
    
+   /**
+    * @return
+    * @throws Exception
+    */
    public List<Location> getAllLocation() throws Exception;
    
-   public List<String> getRecentBibIds() throws Exception;
+   /**
+    * @return
+    * @throws Exception
+    */
+   public List<String> getRecentBibIds(String dateString) throws Exception;
    
-   public BibData getBibData(String bibid) throws Exception;
+   /**
+    * @return
+    * @throws Exception
+    */
+   public int getRecentBibIdCount(String dateString) throws Exception;
+   
+   /**
+    * @param bibid
+    * @return
+    * @throws Exception
+    */
+   public BibBlob getBibBlob(String bibid) throws Exception;
+   
+   /**
+    * @param bibid
+    * @return
+    * @throws Exception
+    */
+   public List<BibData> getBibData(String bibid) throws Exception;
    
 }
