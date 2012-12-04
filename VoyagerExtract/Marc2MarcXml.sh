36def7e578a2bf6f60705ec09cf4313cecd636a5
John Fereira <jaf30@cornell.edu>
2012-12-04 10:48:37 -0500

    change class name to Marc2MarcXml

--- VoyagerExtract/Marc2MarcXml.sh
+++ VoyagerExtract/Marc2MarcXml.sh
@@ -2,7 +2,7 @@
 #
 # Run Marc2MarcXML
 #
-CLASS=edu.cornell.library.integration.Marc2MarcXML
+CLASS=edu.cornell.library.integration.Marc2MarcXml
 CLASSES=./build/classes
 LIB=./build/lib
 
@@ -11,8 +11,8 @@ CLASSPATH=$(JARS=("$LIB"/*.jar); IFS=:; echo "${JARS[*]}")
 CLASSPATH=$CLASSPATH:$CLASSES
 
 # Need to specify src and dest dir on dav
-SRCDIR=http://jaf30-dev.library.cornell.edu/data/voyager/bib/bib.mrc.updates
-DESTDIR=http://jaf30-dev.library.cornell.edu/data/voyager/bib/bib.xml.updates
+SRCDIR=http://jaf30-dev.library.cornell.edu/data/voyager/mfhd/mfhd.mrc.full
+DESTDIR=http://jaf30-dev.library.cornell.edu/data/voyager/mfhd/mfhd.xml.full
 
 java -classpath $CLASSPATH $CLASS $SRCDIR $DESTDIR
 
