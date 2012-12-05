659236dea741d58d18251c057822b8539bf5f257
Brian Caruso <bdc34@cornell.edu>
2012-12-05 11:38:38 -0500

    adding methods to write Model to NTriples String

--- rdfToSolr/src/main/java/edu/cornell/library/integration/hadoop/MarcToSolrUtils.java
+++ rdfToSolr/src/main/java/edu/cornell/library/integration/hadoop/MarcToSolrUtils.java
@@ -3,6 +3,8 @@ package edu.cornell.library.integration.hadoop;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +12,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 public class MarcToSolrUtils {
 	
@@ -33,4 +38,19 @@ public class MarcToSolrUtils {
 		return links;
 	}
 
+	public static String writeModelToNTString(Model m){
+		if( m == null ) return "";		
+		StringWriter writer = new StringWriter();
+		m.write( writer , "N-TRIPLES");				
+		return writer.toString();
+	}	
+	
+	public static Model readNTStringToModel( String str){
+		Model m = ModelFactory.createDefaultModel();
+		if( str == null ) 
+			return m;
+		StringReader reader = new StringReader(str);
+		m.read(reader, null, str);
+		return m;
+	}
 }
