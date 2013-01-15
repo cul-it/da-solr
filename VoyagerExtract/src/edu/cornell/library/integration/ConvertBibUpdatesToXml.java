5646a7d893d8afd7d29ebbbd64db6546a08aca19
John Fereira <jaf30@cornell.edu>
2013-01-15 11:48:38 -0500

    refactor to use Convertutils class.  cleaned up imports

--- VoyagerExtract/src/edu/cornell/library/integration/ConvertBibUpdatesToXml.java
+++ VoyagerExtract/src/edu/cornell/library/integration/ConvertBibUpdatesToXml.java
@@ -1,50 +1,18 @@
 package edu.cornell.library.integration;
 
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.CharArrayWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
-import java.sql.Clob;
-import java.text.SimpleDateFormat;
 
-import oracle.sql.*;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.sax.SAXSource;
-import javax.xml.transform.stream.StreamResult;
-
- 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory; 
-import org.marc4j.marc.Record;
-import org.marc4j.marcxml.Converter;
-import org.marc4j.marcxml.MarcXmlReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.xml.sax.InputSource;
- 
-import edu.cornell.library.integration.bo.BibBlob;
-import edu.cornell.library.integration.bo.BibData; 
-import edu.cornell.library.integration.config.IntegrationDataProperties;
 import edu.cornell.library.integration.ilcommons.service.DavService;
 import edu.cornell.library.integration.ilcommons.service.DavServiceFactory;
 import edu.cornell.library.integration.service.CatalogService; 
@@ -138,31 +106,37 @@ public class ConvertBibUpdatesToXml {
          e.printStackTrace();
       }
       
+      ConvertUtils converter = new ConvertUtils();
+      
       // iterate over mrc files
-      for (String srcFile  : srcList) {
-         System.out.println("Converting mrc file: "+ srcFile);
-			try {
-			   String bibid = StringUtils.replace(srcFile, ".mrc", "");
-			   String mrc = davService.getFileAsString(srcDir + "/" +srcFile); 
-				//System.out.println("mrc: " + mrc);
-				String xml = ConvertUtils.convertMrcToXml(mrc);
-				if (StringUtils.isEmpty(xml)) {
-				   System.out.println("Could not convert file: "+ srcFile);
-               davService.moveFile(srcDir +"/" +srcFile, badDir +"/"+ srcFile);
-				} else { 
-				   saveBibXml(xml, bibid, destDir);
-				}
-			} catch (Exception e) { 
-			   try {
-			      System.out.println("Could not convert file: "+ srcFile);
-			      //e.printStackTrace();
-               davService.moveFile(srcDir +"/" +srcFile, badDir +"/"+ srcFile);
-            } catch (Exception e1) {
-               // TODO Auto-generated catch block
-               e1.printStackTrace();
-            } 
-			}
-		}
+      if (srcList.size() == 0) {
+         System.out.println("No Update Marc files available to process");
+      } else {
+         for (String srcFile  : srcList) {
+            System.out.println("Converting mrc file: "+ srcFile);
+   			try {
+   			   String bibid = StringUtils.replace(srcFile, ".mrc", "");
+   			   String mrc = davService.getFileAsString(srcDir + "/" +srcFile); 
+   				//System.out.println("mrc: " + mrc);
+   				String xml = converter.convertMrcToXml(mrc);
+   				if (StringUtils.isEmpty(xml)) {
+   				   System.out.println("ERROR: Could not convert file: "+ srcFile);
+                  davService.moveFile(srcDir +"/" +srcFile, badDir +"/"+ srcFile);
+   				} else { 
+   				   saveBibXml(xml, bibid, destDir);
+   				}
+   			} catch (Exception e) { 
+   			   try {
+   			      System.out.println("Exception caught: could not convert file: "+ srcFile);
+   			      e.printStackTrace();
+                  davService.moveFile(srcDir +"/" +srcFile, badDir +"/"+ srcFile);
+               } catch (Exception e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+               } 
+   			}
+   		}
+      }
       
    }
    
