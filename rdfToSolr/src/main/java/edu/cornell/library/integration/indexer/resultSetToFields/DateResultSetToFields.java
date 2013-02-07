6c5e2bbb23ac26ab633bce4359826b6725e86763
Frances Webb <frances.webb@cornell.edu>
2013-02-07 10:34:37 -0500

    block 0000 dates from pub_date_facet

--- rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/resultSetToFields/DateResultSetToFields.java
+++ rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/resultSetToFields/DateResultSetToFields.java
@@ -51,23 +51,23 @@ public class DateResultSetToFields implements ResultSetToFields {
 							} else if (resultKey.equals("machine_dates")) {
 								Matcher m = p.matcher(value);
 								if (m.matches()) {
-									if (name.equals("date1")) {
+//									if (name.equals("date1")) {
 										int year = Integer.valueOf(value);
-										if ((year < 9999) && ! found_single_date) {
+										if ((year > 0) && (year < 9999) && ! found_single_date) {
 											addField(fields,"pub_date_sort",value);
 //											addField(fields,"pub_date",value);
 											addField(fields,"pub_date_facet",value);
 											found_single_date = true;
 										}
-									} else {
+/*									} else {
 										int year = Integer.valueOf(value);
-										if ((year < 9999) && ! found_single_date) {
+										if ((year > 0) && (year < 9999) && ! found_single_date) {
 											addField(fields,"pub_date_sort",value);
 //											addField(fields,"pub_date",value);
 											addField(fields,"pub_date_facet",value);
 											found_single_date = true;
 										}
-									}
+									}*/
 								} else {
 								// debug stmt should be added. Node is not a date in \d\d\d\d format.
 								}
