dd9917fdee2b3eb59161043ddadaecb45159d479
U-CORNELL\jaf30 <jaf30@LIB-JAF30.library.cornell.edu>
2013-02-17 09:40:52 -0500

    initial version

---
+++ VoyagerExtract/ConvertMfhdFullToXml.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CLASS=edu.cornell.library.integration.ConvertMfhdFullToXml
+CLASSES=./build/classes
+LIB=./build/lib
+
+
+CLASSPATH=$(JARS=("$LIB"/*.jar); IFS=:; echo "${JARS[*]}")
+CLASSPATH=$CLASSPATH:$CLASSES
+
+# need to specifiy src and destination Dir for mfhd data
+# java -classpath $CLASSPATH $CLASS $@
+SRCDIR=http://culdata.library.cornell.edu/data/voyager/mfhd/mfhd.mrc.full
+DESTDIR=http://culdata.library.cornell.edu/data/voyager/mfhd/mfhd.xml.full
+java -classpath $CLASSPATH $CLASS $SRCDIR $DESTDIR
