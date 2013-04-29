f4aebe2f44e846e1e443db09eba14aa6b8606013
John Fereira <jaf30@cornell.edu>
2013-04-29 14:24:54 -0400

    first commit

---
+++ VoyagerExtract/src/edu/cornell/library/integration/support/ShowArgs.java
@@ -0,0 +1,16 @@
+package edu.cornell.library.integration.support;
+
+public class ShowArgs {
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+      for (int i=0; i < args.length ; i++) {
+         System.out.println(i+ " : "+ args[i]);
+      }
+
+   }
+
+}
