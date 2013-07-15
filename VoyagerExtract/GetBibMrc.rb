712a728989504b767a537c0a57c735a2cf23906a
John Fereira <jaf30@cornell.edu>
2013-07-15 10:06:23 -0400

    pick up bibid as arg

--- VoyagerExtract/GetBibMrc.rb
+++ VoyagerExtract/GetBibMrc.rb
@@ -9,7 +9,8 @@ Dir["./lib/\*.jar"].each { |jar| require jar }
 # print $CLASSPATH
 java_import 'edu.cornell.library.integration.GetBibMrc'
 print "GetBibMrc...\n" 
-bibid = "5430043"
+# bibid = "5430043"
+bibid = ARGV[0]
 destdir = "http://culdata.library.cornell.edu/data/voyager/bib/bib.mrc.updates" 
 args = [bibid,destdir].to_java(:string)
 GetBibMrc.main(args)
