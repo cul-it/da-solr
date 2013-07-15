a8ba62f8825ba2331cdca1b5ee1d2ea827217e2e
John Fereira <jaf30@cornell.edu>
2013-07-15 15:25:26 -0400

    first commit

---
+++ VoyagerExtract/GetBibBatchMrc.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env jruby
+#
+# GetBibBatchMrc
+#
+require "java"
+$CLASSPATH << 'build/classes/'
+Dir["./lib/\*.jar"].each { |jar| require jar }
+
+# print $CLASSPATH
+java_import 'edu.cornell.library.integration.GetBibBatchMrc'
+print "GetBibBatchMrc...\n" 
+filename = ARGV[0]
+destdir = "http://culdata.library.cornell.edu/data/voyager/bib/bib.mrc.updates" 
+args = [filename,destdir].to_java(:string)
+GetBibBatchMrc.main(args)
