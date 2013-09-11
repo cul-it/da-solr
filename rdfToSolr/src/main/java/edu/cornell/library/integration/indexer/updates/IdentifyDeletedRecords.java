ab5b929911f469fa196691a763058298022956a4
Frances Webb <frances.webb@cornell.edu>
2013-09-11 12:03:07 -0400

    webdav and process updates to step 2

--- rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/updates/IdentifyDeletedRecords.java
+++ rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/updates/IdentifyDeletedRecords.java
@@ -1,36 +1,147 @@
 package edu.cornell.library.integration.indexer.updates;
 
+import java.io.ByteArrayInputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import edu.cornell.library.integration.indexer.utilies.*;
+import edu.cornell.library.integration.ilcommons.service.DavService;
+import edu.cornell.library.integration.ilcommons.service.DavServiceFactory;
+import edu.cornell.library.integration.indexer.utilies.IndexRecordListComparison;
 
 public class IdentifyDeletedRecords {
+	
+	private final String davUrl = "http://culdatadev.library.cornell.edu/data";
 
+	DavService davService;
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		//should be args
 		String coreUrl = "http://fbw4-dev.library.cornell.edu:8080/solr/test";
-		Path currentVoyagerBibList = Paths.get("/users/fbw4/voyager-harvest/data/fulldump/bibs/unsuppressedBibId.txt");
-		Path currentVoyagerMfhdList = Paths.get("/users/fbw4/voyager-harvest/data/fulldump/mfhds/unsuppressedMfhdId.txt");
+		if (args.length >= 1)
+			coreUrl = args[0];
+		if (args.length >= 3) {
+			Path currentVoyagerBibList = Paths.get(args[1]);
+			Path currentVoyagerMfhdList = Paths.get(args[2]);
+			new IdentifyDeletedRecords(coreUrl,currentVoyagerBibList,currentVoyagerMfhdList);
+		} else 
+			new IdentifyDeletedRecords(coreUrl);
+	}
+	
+	public IdentifyDeletedRecords(String coreUrl, Path currentVoyagerBibList, Path currentVoyagerMfhdList) {
+
+		davService = DavServiceFactory.getDavService();
 
 		IndexRecordListComparison c = new IndexRecordListComparison();
 		c.compare(coreUrl, currentVoyagerBibList, currentVoyagerMfhdList);
+		
+		produceReport(c);
+
+	}
+
+	public IdentifyDeletedRecords(String coreUrl) {
+		davService = DavServiceFactory.getDavService();
+		Path currentVoyagerBibList = null;
+		Path currentVoyagerMfhdList = null;
+		try {
+			List<String> biblists = davService.getFileList(davUrl+"/voyager/bib/unsuppressed");
+			Pattern p = Pattern.compile("unsuppressedBibId-(....-..-..).txt"); 
+			Iterator<String> i = biblists.iterator();
+			Date lastDate = new SimpleDateFormat("yyyy").parse("1950");
+			String mostRecentBibFile = null;
+			while (i.hasNext()) {
+				String fileName = i.next();
+				Matcher m = p.matcher(fileName);
+				if (m.matches()) {
+					Date thisDate = new SimpleDateFormat("yyyy-mm-dd").parse(m.group(1));
+					if (thisDate.after(lastDate)) {
+						lastDate = thisDate;
+						mostRecentBibFile = fileName;
+					}
+				}
+			}
+			if (mostRecentBibFile != null) {
+				System.out.println("Most recent bib file identified as: "+davUrl+"/voyager/bib/unsuppressed/"+mostRecentBibFile);
+				currentVoyagerBibList = Paths.get(davUrl+"/voyager/bib/unsuppressed/"+mostRecentBibFile);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			List<String> mfhdlists = davService.getFileList(davUrl+"/voyager/mfhd/unsuppressed");
+			Pattern p = Pattern.compile("unsuppressedMfhdId-(....-..-..).txt"); 
+			Iterator<String> i = mfhdlists.iterator();
+			Date lastDate = new SimpleDateFormat("yyyy").parse("1950");
+			String mostRecentMfhdFile = null;
+			while (i.hasNext()) {
+				String fileName = i.next();
+				Matcher m = p.matcher(fileName);
+				if (m.matches()) {
+					Date thisDate = new SimpleDateFormat("yyyy-mm-dd").parse(m.group(1));
+					if (thisDate.after(lastDate)) {
+						lastDate = thisDate;
+						mostRecentMfhdFile = fileName;
+					}
+				}
+			}
+			if (mostRecentMfhdFile != null) {
+				System.out.println("Most recent mfhd file identified as: "+davUrl+"/voyager/mfhd/unsuppressed/"+mostRecentMfhdFile);
+				currentVoyagerMfhdList = Paths.get(davUrl+"/voyager/mfhd/unsuppressed/"+mostRecentMfhdFile);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if ((currentVoyagerBibList != null) && (currentVoyagerMfhdList != null)) {
+			System.out.println("Lists identified. Moving on.");
+
+//			IndexRecordListComparison c = new IndexRecordListComparison();
+//			c.compare(coreUrl, currentVoyagerBibList, currentVoyagerMfhdList);
+			
+//			printReport(c);
+		}
+ 	}
+
+	// Based on the IndexRecordListComparison, bib records to be updated and deleted are printed to STDOUT
+	// and also written to report files on the webdav server in the /updates/bib.deletes and /updates/bibupdates
+	// folders. The report files have post-pended dates in their file names.
+	private void produceReport( IndexRecordListComparison c ) {
+
+		String currentDate = new SimpleDateFormat("yyyy-mm-dd").format(new Date());
 
 		// bibs in index not voyager
 		if (c.bibsInIndexNotVoyager.size() > 0) {
 			System.out.println("bibids to be deleted from Solr");
 			Integer[] ids = c.bibsInIndexNotVoyager.toArray(new Integer[ c.bibsInIndexNotVoyager.size() ]);
 			Arrays.sort( ids );
-			for( Integer id: ids )
+
+			StringBuilder sb = new StringBuilder();
+			for( Integer id: ids ) {
 				System.out.println(id);
+				sb.append(id);
+				sb.append("\n");
+			}
+
+			String deleteReport = sb.toString();
+			try {
+				davService.saveFile(davUrl+"/updates/bib.deletes/bibListForDelete-"+ currentDate + ".txt",
+						new ByteArrayInputStream(deleteReport.getBytes("UTF-8")));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
 		}
 
 		
@@ -42,11 +153,25 @@ public class IdentifyDeletedRecords {
 			while (bibids.hasNext())
 				update_bibids.add(bibids.next());
 			bibids = update_bibids.iterator();
-			while (bibids.hasNext())
-				System.out.println(bibids.next());
+			StringBuilder sb = new StringBuilder();
+			while (bibids.hasNext()) {
+				Integer bibid = bibids.next();
+				System.out.println(bibid);
+				sb.append(bibid);
+				sb.append("\n");
+			}
+
+			String updateReport = sb.toString();
+			try {
+				davService.saveFile(davUrl+"/updates/bib.updates/bibListForUpdate-"+ currentDate + ".txt",
+						new ByteArrayInputStream(updateReport.getBytes("UTF-8")));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		
 	}
-
+	
 }
