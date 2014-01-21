fbe97d42bce81a50f735f8d93cb9002d863be396
Frances Webb <frances.webb@cornell.edu>
2014-01-21 16:01:29 -0500

    start of a debug function to identify source of stdout output.

---
+++ ILCommons/src/main/java/edu/cornell/library/integration/ilcommons/util/TraceOutput.java
@@ -0,0 +1,25 @@
+package edu.cornell.library.integration.ilcommons.util;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class TraceOutput extends PrintStream {
+
+	public TraceOutput(OutputStream out) {
+		super(out);
+	}
+
+	public void println(Object o) {
+		StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+		super.println(o);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
