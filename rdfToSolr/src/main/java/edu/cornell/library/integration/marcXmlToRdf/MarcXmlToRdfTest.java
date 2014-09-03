771daa2668f070945e790527362b785c77b8bbf8
Frances Webb <frances.webb@cornell.edu>
2014-09-03 18:12:51 -0400

    replace accidentally dropped files

---
+++ rdfToSolr/src/main/java/edu/cornell/library/integration/marcXmlToRdf/MarcXmlToRdfTest.java
@@ -0,0 +1,18 @@
+package edu.cornell.library.integration.marcXmlToRdf;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MarcXmlToRdfTest {
+
+	@Test
+	public void escapeForNTriplesTest() {
+		String s1 = "test \\ string";
+		String s2 = MarcXmlToRdf.escapeForNTriples( s1 );
+		assertTrue(s2.equals("test \\\\ string"));
+		s1 = "testing \"quotes\".";
+		s2 = MarcXmlToRdf.escapeForNTriples( s1 );
+		assertTrue(s2.equals("testing \\\"quotes\\\"."));
+	}
+
+}
