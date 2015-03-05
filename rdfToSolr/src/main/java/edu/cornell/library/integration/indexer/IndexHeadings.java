523be3d1f8bf491db30c2159b030133c242fff4b
Frances Webb <frances.webb@cornell.edu>
2015-03-05 14:43:19 -0500

    finished index heading revamp (v.1)

--- rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/IndexHeadings.java
+++ rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/IndexHeadings.java
@@ -1,23 +1,31 @@
 package edu.cornell.library.integration.indexer;
 
-import static edu.cornell.library.integration.indexer.resultSetToFields.ResultSetUtilities.removeAllPunctuation;
+import static edu.cornell.library.integration.indexer.utilities.BrowseUtils.getSortHeading;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.events.XMLEvent;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.solr.client.solrj.SolrServerException;
 
 import edu.cornell.library.integration.ilcommons.configuration.VoyagerToSolrConfiguration;
+import edu.cornell.library.integration.indexer.utilities.BrowseUtils.BlacklightField;
 import edu.cornell.library.integration.indexer.utilities.BrowseUtils.HeadType;
 import edu.cornell.library.integration.indexer.utilities.BrowseUtils.HeadTypeDesc;
 import edu.cornell.library.integration.indexer.utilities.BrowseUtils.RecordSet;
@@ -25,59 +33,60 @@ import edu.cornell.library.integration.indexer.utilities.BrowseUtils.RecordSet;
 public class IndexHeadings {
 
 	private Connection connection = null;
-	private PreparedStatement ps_recordsByQuery = null;
-	private PreparedStatement ps_recordsAboutQuery = null;
-	private PreparedStatement ps_recordsQuery = null;
-	private PreparedStatement ps_createHeadingWBy = null;
-	private PreparedStatement ps_createHeadingWAbout = null;
-	private PreparedStatement ps_createHeadingWWorksCount = null;
-	private PreparedStatement ps_updateRecordsBy = null;
-	private PreparedStatement ps_updateRecordsAbout = null;
-	private PreparedStatement ps_updateRecords = null;
+	// This structure should contain only up to six PreparedStatement objects at most.
+	private Map<HeadType,Map<String,PreparedStatement>> statements =
+			new HashMap<HeadType,Map<String,PreparedStatement>>();
+	VoyagerToSolrConfiguration config;
+	private Map<Integer,Integer> wrongHeadingCounts = new HashMap<Integer,Integer>();
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		try {
+			new IndexHeadings(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	public IndexHeadings(String[] args) throws Exception {
+        
 		// load configuration for location of index, location of authorities
 		Collection<String> requiredArgs = new HashSet<String>();
 		requiredArgs.add("xmlDir");
 		requiredArgs.add("blacklightSolrUrl");
 		requiredArgs.add("solrUrl");
 	            
-		VoyagerToSolrConfiguration config = VoyagerToSolrConfiguration.loadConfig(args,requiredArgs);
-		try {
-			new IndexHeadings(config);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
-	
-	
-	public IndexHeadings(VoyagerToSolrConfiguration config) throws Exception {
-        
-		connection = config.getDatabaseConnection(1);
-		Collection<BLField> blFields = new HashSet<BLField>();
-		blFields.add(new BLField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.PERSNAME, "author_100_exact" ));
-		blFields.add(new BLField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.CORPNAME, "author_110_exact" ));
-		blFields.add(new BLField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.EVENT,    "author_111_exact" ));
-		blFields.add(new BLField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.PERSNAME, "author_700_exact" ));
-		blFields.add(new BLField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.CORPNAME, "author_710_exact" ));
-		blFields.add(new BLField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.EVENT,    "author_711_exact" ));
-		blFields.add(new BLField(RecordSet.NAME, HeadType.SUBJECT, HeadTypeDesc.PERSNAME, "subject_600_exact"));
-		blFields.add(new BLField(RecordSet.NAME, HeadType.SUBJECT, HeadTypeDesc.CORPNAME, "subject_610_exact"));
-		blFields.add(new BLField(RecordSet.NAME, HeadType.SUBJECT, HeadTypeDesc.EVENT, "subject_611_exact"));
-		blFields.add(new BLField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.TOPIC, "subject_650_exact"));
-		blFields.add(new BLField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.GEONAME, "subject_651_exact"));
-		blFields.add(new BLField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.CHRONTERM, "subject_648_exact"));
-		blFields.add(new BLField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.GENRE, "subject_655_exact"));
-		blFields.add(new BLField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.GEONAME, "subject_662_exact"));
+		config = VoyagerToSolrConfiguration.loadConfig(args,requiredArgs);		
 		
-		for (BLField blf : blFields) {
+		connection = config.getDatabaseConnection(1);
+		Collection<BlacklightField> blFields = new HashSet<BlacklightField>();
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.PERSNAME, "author_100_exact","author_facet" ));
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.CORPNAME, "author_110_exact","author_facet" ));
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.EVENT,    "author_111_exact","author_facet" ));
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.PERSNAME, "author_700_exact","author_facet" ));
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.CORPNAME, "author_710_exact","author_facet" ));
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.AUTHOR, HeadTypeDesc.EVENT,    "author_711_exact","author_facet" ));
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.SUBJECT, HeadTypeDesc.PERSNAME, "subject_600_exact","subject_topic_facet"));
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.SUBJECT, HeadTypeDesc.CORPNAME, "subject_610_exact","subject_topic_facet"));
+		blFields.add(new BlacklightField(RecordSet.NAME, HeadType.SUBJECT, HeadTypeDesc.EVENT, "subject_611_exact","subject_topic_facet"));
+		blFields.add(new BlacklightField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.TOPIC, "subject_650_exact","subject_topic_facet"));
+		blFields.add(new BlacklightField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.GEONAME, "subject_651_exact","subject_geo_facet"));
+		blFields.add(new BlacklightField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.CHRONTERM, "subject_648_exact","subject_era_facet"));
+		blFields.add(new BlacklightField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.GENRE, "subject_655_exact","subject_topic_facet"));
+		blFields.add(new BlacklightField(RecordSet.SUBJECT, HeadType.SUBJECT, HeadTypeDesc.GEONAME, "subject_662_exact","subject_topic_facet"));//TODO: switch to geo after reindex
+		
+		for (BlacklightField blf : blFields) {
 		
 			System.out.printf("Poling Blacklight Solr field %s for %s values as %s\n",
 						blf.fieldName(),blf.headingTypeDesc(),blf.headingType());
+			
+			if ( ! statements.containsKey(blf.headingType()))
+				statements.put(blf.headingType(), new HashMap<String,PreparedStatement>());
+			
 			URL queryUrl = new URL(config.getBlacklightSolrUrl() + "/terms?terms.fl=" +
 					blf.fieldName() + "&terms.sort=index&terms.limit=100000000");
 			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
@@ -109,7 +118,7 @@ public class IndexHeadings {
 							if (r.getAttributeLocalName(i).equals("name"))
 								heading = r.getAttributeValue(i);
 						recordCount = Integer.valueOf(r.getElementText());
-						addCountToDB(blf,heading,recordCount);
+						addCountToDB(blf,statements.get(blf.headingType()),heading, recordCount);
 						if (++headingCount % 10_000 == 0)
 							System.out.printf("%s => %d\n",heading,recordCount);
 					}
@@ -121,89 +130,122 @@ public class IndexHeadings {
 	}
 
 	
-	private void addCountToDB(BLField blf, String heading, Integer count) throws SolrServerException, SQLException {
+	private void addCountToDB(BlacklightField blf, Map<String, PreparedStatement> stmts, String headingSort, Integer count) throws SolrServerException, SQLException {
 
-		PreparedStatement pstmt = null;
-		String count_field = null;
-		if (blf.headingType().equals(HeadType.AUTHOR)) {
-			count_field = "works_by";
-			if (ps_recordsByQuery == null) 
-				ps_recordsByQuery = connection.prepareStatement(
-						"SELECT id, works_by as count FROM heading " +
-						"WHERE type = ? AND type_desc = ? AND sort = ?");
-			pstmt = ps_recordsByQuery;
-		} else if (blf.headingType().equals(HeadType.SUBJECT)) {
-			count_field = "works_about";
-			if (ps_recordsAboutQuery == null) 
-				ps_recordsAboutQuery = connection.prepareStatement(
-						"SELECT id, works_about as count FROM heading " +
-						"WHERE type = ? AND type_desc = ? AND sort = ?");
-			pstmt = ps_recordsAboutQuery;
-		} else {
-			count_field = "works";
-			if (ps_recordsQuery == null) 
-				ps_recordsQuery = connection.prepareStatement(
-						"SELECT id, works as count FROM heading " +
-						"WHERE type = ? AND type_desc = ? AND sort = ?");
-			pstmt = ps_recordsQuery;
+		String count_field = blf.headingType().dbField();
+		// update record count in db
+		if ( ! stmts.containsKey("update")) {
+			String query = String.format( "UPDATE heading SET %s = %s + ? "
+					+ "WHERE record_set = ? AND type_desc = ? AND sort = ?", count_field, count_field);
+			stmts.put("update", connection.prepareStatement(query));
 		}
+		PreparedStatement stmt = stmts.get("update");
+		stmt.setInt(1, count);
+		stmt.setInt(2, blf.recordSet().ordinal());
+		stmt.setInt(3, blf.headingTypeDesc().ordinal());
+		stmt.setString(4, headingSort);
+		int rowsAffected = stmt.executeUpdate();
 		
-		String headingSort = getSortHeading( heading );
-
-		pstmt.setInt(1, blf.headingType().ordinal());
-		pstmt.setInt(2, blf.headingTypeDesc().ordinal());
-		pstmt.setString(3, getSortHeading(heading));
-		ResultSet rs = pstmt.executeQuery();
-		Integer recordId = null;
-		Integer oldCount = null;
-		while (rs.next()) {
-			recordId = rs.getInt("id");
-			oldCount = rs.getInt("count");
-		}
-
-		if (recordId != null) {
-			// Update record count
-		} else {
-			// create new record
-			if (count_field.equals("works_by")) {
-				if (ps_createHeadingWBy == null)
-					ps_createHeadingWBy = connection.prepareStatement(
-							"INSERT INTO heading (heading, sort, type, type_desc, works_by) " +
-							"VALUES (?, ?, ?, ?, ?)");
-				pstmt = ps_createHeadingWBy;
-			} else if (count_field.equals("works_about")) {
-				if (ps_createHeadingWAbout == null)
-					ps_createHeadingWAbout = connection.prepareStatement(
-							"INSERT INTO heading (heading, sort, type, type_desc, works_about) " +
-							"VALUES (?, ?, ?, ?, ?)");
-				pstmt = ps_createHeadingWAbout;
-			} else {
-				if (ps_createHeadingWWorksCount == null)
-					ps_createHeadingWWorksCount = connection.prepareStatement(
-							"INSERT INTO heading (heading, sort, type, type_desc, works) " +
-							"VALUES (?, ?, ?, ?, ?)");
-				pstmt = ps_createHeadingWWorksCount;
+		// if no rows were affected, this heading is not yet in the database
+		if ( rowsAffected == 0 ) {
+			String headingDisplay;
+			try {
+				headingDisplay = getDisplayHeading( blf , headingSort );
+				if ( ! stmts.containsKey("insert")) {
+					String query = String.format(
+							"INSERT INTO heading (heading, sort, record_set, type_desc, %s) " +
+							"VALUES (?, ?, ?, ?, ?)", count_field);
+					stmts.put("insert", connection.prepareStatement(query));
+				}
+				stmt = stmts.get("insert");
+				stmt.setString(1, headingDisplay);
+				stmt.setString(2, headingSort);
+				stmt.setInt(3, blf.recordSet().ordinal());
+				stmt.setInt(4, blf.headingTypeDesc().ordinal());
+				stmt.setInt(5, count);
+				stmt.executeUpdate();
+			} catch (IOException | XMLStreamException | URISyntaxException e) {
+				System.out.println("IO error retrieving heading display format from Blacklight. Count not recorded for: "+headingSort);
+				e.printStackTrace();
+				System.exit(1);
 			}
-			pstmt.setString(1, heading);
-			pstmt.setString(2, headingSort);
-			pstmt.setInt(3, blf.headingType().ordinal());
-			pstmt.setInt(4, blf.headingTypeDesc().ordinal());
-			pstmt.setInt(5, count);
-			int affectedCount = pstmt.executeUpdate();
-			if (affectedCount < 1) 
-				throw new SQLException("Creating Heading Record Failed.");
 		}
+
 	}
 
 	
-	private String getSortHeading(String heading) {
-		// Remove all punctuation will strip punctuation. We replace hyphens with spaces
-		// first so hyphenated words will sort as though the space were present.
-		String sortHeading = removeAllPunctuation(heading.
-				replaceAll("\\p{InCombiningDiacriticalMarks}+", "").
-				toLowerCase().
-				replaceAll("-", " "));
-		return sortHeading.trim();
+	private String getDisplayHeading(BlacklightField blf, String headingSort) throws IOException, XMLStreamException, URISyntaxException {
+		
+		String facet = blf.facetField();
+		if (facet == null)
+			return headingSort;
+
+		Collection<String> solrArgs = new HashSet<String>();
+		solrArgs.add( "qt=standard" );
+		solrArgs.add( "q="+URLEncoder.encode("*:*","UTF-8") );
+		solrArgs.add( "fq="+
+				URLEncoder.encode(String.format("%s:\"%s\"",blf.fieldName(),headingSort),"UTF-8"));
+		solrArgs.add( "rows=0" );
+		solrArgs.add( "echoParams=none" );
+		solrArgs.add( "facet=true" );
+		solrArgs.add( "facet.field="+facet );
+		solrArgs.add( "facet.limit=40" );
+		solrArgs.add( "facet.mincount=1" );
+		String query = config.getBlacklightSolrUrl() +"/select?" + StringUtils.join( solrArgs, "&");
+//		System.out.println("**** Looking for display value for: "+headingSort+"****");
+		
+
+		URI uri = new URI(query);
+		URL queryUrl = uri.toURL();
+		XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+		InputStream in = queryUrl.openStream();
+		XMLStreamReader r  = inputFactory.createXMLStreamReader(in);
+
+		// fast forward to response body
+		FF: while (r.hasNext()) {
+			String event = getEventTypeString(r.next());
+			if (event.equals("START_ELEMENT"))
+				if (r.getLocalName().equals("lst"))
+					for (int i = 0; i < r.getAttributeCount(); i++)
+						if (r.getAttributeLocalName(i).equals("name"))
+							if (r.getAttributeValue(i).equals(facet)) break FF;
+		}
+		
+		// process actual results
+		String heading = null;
+		int wrongHeadingCount = 0;
+		while (r.hasNext()) {
+			String event = getEventTypeString(r.next());
+			if (event.equals("START_ELEMENT")) {
+				if (r.getLocalName().equals("int")) {
+					for (int i = 0; i < r.getAttributeCount(); i++)
+						if (r.getAttributeLocalName(i).equals("name"))
+							heading = r.getAttributeValue(i);
+					String sort = getSortHeading(heading);
+//					System.out.println(heading + " => "+ sort);
+					if (sort.equals(headingSort)) {
+						in.close();
+						recordWrongHeadingCount(wrongHeadingCount);
+						return heading;
+					} else {
+						wrongHeadingCount++;
+					}
+				}
+			}
+		}
+		in.close();
+		recordWrongHeadingCount(wrongHeadingCount);
+		System.out.println("Didn't find display form for: "+headingSort);
+		System.out.println(query);
+	//	System.exit(1);
+		return headingSort;
+	}
+	
+	private void recordWrongHeadingCount( int c ) {
+		if (wrongHeadingCounts.containsKey(c))
+			wrongHeadingCounts.put(c, wrongHeadingCounts.get(c)+1);
+		else
+			wrongHeadingCounts.put(c, 1);
 	}
 
 	private final static String getEventTypeString(int  eventType)
@@ -238,24 +280,5 @@ public class IndexHeadings {
 	  return  "UNKNOWN_EVENT_TYPE ,   "+ eventType;
 	}
 	
-	static class BLField {
-		private RecordSet _rs;
-		private HeadType _ht;
-		private HeadTypeDesc _htd;
-		private String _fieldName;
-		
-		
-		public BLField(RecordSet rs, HeadType ht, HeadTypeDesc htd,String fieldName) {
-			_ht = ht;
-			_htd = htd;
-			_fieldName = fieldName;
-			_rs = rs;
-		}
-		public RecordSet recordSet() { return _rs; }
-		public HeadType headingType() { return _ht; }
-		public HeadTypeDesc headingTypeDesc() { return _htd; }
-		public String fieldName() { return _fieldName; }
-		
-	}
 
 }
--- rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/utilities/BrowseUtils.java
+++ rdfToSolr/src/main/java/edu/cornell/library/integration/indexer/utilities/BrowseUtils.java
@@ -26,18 +26,45 @@ public class BrowseUtils {
 		public String toString() { return string; }
 	}
 	
+	public static class BlacklightField {
+		private RecordSet _rs;
+		private HeadType _ht;
+		private HeadTypeDesc _htd;
+		private String _field;
+		private String _facet;
+		
+		
+		public BlacklightField(RecordSet rs, HeadType ht, HeadTypeDesc htd,String field, String facetField) {
+			_ht = ht;
+			_htd = htd;
+			_field = field;
+			_facet = facetField;
+			_rs = rs;
+		}
+		public RecordSet recordSet() { return _rs; }
+		public HeadType headingType() { return _ht; }
+		public HeadTypeDesc headingTypeDesc() { return _htd; }
+		public String fieldName() { return _field; }
+		public String facetField() { return _facet; }
+		
+	}
+
+	
 	public static enum HeadType {
-		AUTHOR("author"),
-		SUBJECT("subject"),
-		AUTHORTITLE("authortitle");
+		AUTHOR("author","works_by"),
+		SUBJECT("subject","works_about"),
+		AUTHORTITLE("authortitle","works");
 		
 		private String string;
+		private String field;
 		
-		private HeadType(String name) {
+		private HeadType(String name, String dbField) {
 			string = name;
+			field = dbField;
 		}
 
 		public String toString() { return string; }
+		public String dbField() { return field; }
 	}	
 
 	public static enum RecordSet {
