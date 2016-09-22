9d2d126b9db8ea923fda11210f273d440f666872
Frances Webb <frances.webb@cornell.edu>
2016-09-22 15:25:03 -0400

    Don't stop iterative updates while the queue isn't empty

--- src/main/java/edu/cornell/library/integration/indexer/updates/IterativeUpdateFromVoyager.java
+++ src/main/java/edu/cornell/library/integration/indexer/updates/IterativeUpdateFromVoyager.java
@@ -2,12 +2,21 @@ package edu.cornell.library.integration.indexer.updates;
 
 import static edu.cornell.library.integration.utilities.IndexingUtilities.commitIndexChanges;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Calendar;
 import java.util.List;
 
 import edu.cornell.library.integration.ilcommons.configuration.SolrBuildConfig;
+import edu.cornell.library.integration.utilities.DaSolrUtilities.CurrentDBTable;
 
 public class IterativeUpdateFromVoyager {
+
+	private final static String isQueueRemainingQuery = "SELECT * FROM "+CurrentDBTable.QUEUE+
+			" WHERE done_date = 0 AND batched_date = 0 AND priority = 0 LIMIT 1";
+
 	public static void main(String[] args) throws Exception {
 		List<String> requiredArgs = SolrBuildConfig.getRequiredArgsForDB("Current");
 		requiredArgs.addAll(SolrBuildConfig.getRequiredArgsForDB("Voy"));
@@ -36,7 +45,8 @@ public class IterativeUpdateFromVoyager {
 		System.out.println("Processing updates to Voyager until: "+quittingTime+":00.");
 
 		int i = 0;
-		while (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < quittingTime) {
+		while (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < quittingTime
+				|| isQueueRemaining(config) ) {
 
 			config.setWebdavBaseUrl(webdavBaseURL + "/" + (++i) );
 			config.setLocalBaseFilePath(localBaseFilePath + "/" + i);
@@ -48,6 +58,17 @@ public class IterativeUpdateFromVoyager {
 			new IncrementalBibFileToSolr(config);
 			commitIndexChanges( config.getSolrUrl() );
 		}
+	}
+
+	private static boolean isQueueRemaining(SolrBuildConfig config) throws ClassNotFoundException, SQLException {
+		boolean remaining = false;
+		try ( Connection current = config.getDatabaseConnection("Current");
+				Statement stmt = current.createStatement();
+				ResultSet rs = stmt.executeQuery( isQueueRemainingQuery )) {
+			while (rs.next())
+				remaining = true;
+		}
+		return remaining;
 	}
 
 }
