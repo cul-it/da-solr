3d20078e9b174c50096baebc48fc7ce887100edd
Frances Webb <frances.webb@cornell.edu>
2016-10-20 14:57:00 -0400

    Fix broken Unicode in tests

--- src/test/java/edu/cornell/library/integration/utilities/CharacterSetUtilsTest.java
+++ src/test/java/edu/cornell/library/integration/utilities/CharacterSetUtilsTest.java
@@ -12,41 +12,41 @@ import org.junit.Test;
 public class CharacterSetUtilsTest {
 	
 	@Test
-	public static void testIsCJK() {
-		assertFalse(isCJK("Ø§Ù„Ù‚Ø§Ù‡Ø±Ø© : Ø´Ù…Ø³ Ù„Ù„Ù†Ø´Ø± ÙˆØ§Ù„ØªÙˆØ²ÙŠØ¹ØŒ 2012.â€�â€¬â€Ž")); // Arabic publication info
+	public void testIsCJK() {
+		assertFalse(isCJK("القاهرة : شمس للنشر والتوزيع، 2012.‏‬‎")); // Arabic publication info
 		//Japanese title with English Translation
-		assertTrue(isCJK("è«–ç©¶ã‚¸ãƒ¥ãƒªã‚¹ãƒˆ = Quarterly jurist"));
-		assertTrue(isCJK("ä¸€ä¹�äº”ã€‡å¹´ä»£çš„å�°ç�£")); // Chinese Title
+		assertTrue(isCJK("論究ジュリスト = Quarterly jurist"));
+		assertTrue(isCJK("一九五〇年代的台灣")); // Chinese Title
 		assertFalse(isCJK("Saint Antoine le Grand dans l'Orient chretien")); // French Title
-		assertTrue(isCJK("ì œêµ­ ì�˜ ìœ„ì•ˆë¶€")); // Korean Title
+		assertTrue(isCJK("제국 의 위안부")); // Korean Title
 		assertFalse(isCJK("Advanced linear algebra"));  // English Title
 		//English text with Japanese character sometimes used as emoticon
-		assertFalse(isCJK("Hi! How are you? ã‚·"));
-	}
+		assertFalse(isCJK("Hi! How are you? シ"));
+}
 	
 	/*
 	 * We expect the same results from hasCJK() as isCJK(), except for the final test,
 	 * which should return true instead of false.
 	 */
 	@Test
-	public static void testHasCJK() {
-		assertFalse(hasCJK("Ø§Ù„Ù‚Ø§Ù‡Ø±Ø© : Ø´Ù…Ø³ Ù„Ù„Ù†Ø´Ø± ÙˆØ§Ù„ØªÙˆØ²ÙŠØ¹ØŒ 2012.â€�â€¬â€Ž")); 
-		assertTrue(hasCJK("è«–ç©¶ã‚¸ãƒ¥ãƒªã‚¹ãƒˆ = Quarterly jurist"));
-		assertTrue(hasCJK("ä¸€ä¹�äº”ã€‡å¹´ä»£çš„å�°ç�£"));
+	public void testHasCJK() {
+		assertFalse(hasCJK("القاهرة : شمس للنشر والتوزيع، 2012.‏‬‎")); 
+		assertTrue(hasCJK("論究ジュリスト = Quarterly jurist"));
+		assertTrue(hasCJK("一九五〇年代的台灣"));
 		assertFalse(hasCJK("Saint Antoine le Grand dans l'Orient chretien"));
-		assertTrue(hasCJK("ì œêµ­ ì�˜ ìœ„ì•ˆë¶€"));
+		assertTrue(hasCJK("제국 의 위안부"));
 		assertFalse(hasCJK("Advanced linear algebra"));
-		assertTrue(hasCJK("Hi! How are you? ã‚·"));
-	}
+		assertTrue(hasCJK("Hi! How are you? シ"));
+}
 	
 	@Test
-	public static void testTrimInternationally() {
+	public void testTrimInternationally() {
 		// tests involving standard ASCII spacing
 		assertTrue(trimInternationally(" abc ").equals("abc"));
 		assertTrue(trimInternationally("   ").equals(""));
 		assertTrue(trimInternationally("\tHello,   World !\n").equals("Hello,   World !"));
 		assertTrue(trimInternationally("Hello, World.").equals("Hello, World."));
 		// CJK spacing
-		assertTrue(trimInternationally("ã€€ã€€ä¿„å›½ã€€ä¸œæ­£æ•™ã€€ä¾µã€€å�Žã€€å�²ç•¥ã€€").equals("ä¿„å›½ã€€ä¸œæ­£æ•™ã€€ä¾µã€€å�Žã€€å�²ç•¥"));
+		assertTrue(trimInternationally("　　俄国　东正教　侵　华　史略　").equals("俄国　东正教　侵　华　史略"));
 	}
 }
