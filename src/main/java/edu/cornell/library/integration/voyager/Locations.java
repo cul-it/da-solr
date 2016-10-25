791c4a9128d8ca4372ca53d1daa2efb7a82d2ca6
Frances Webb <frances.webb@cornell.edu>
2016-10-25 16:13:17 -0400

    Add Javadoc comments, and minor clean-up
    DISCOVERYACCESS-2720

--- src/main/java/edu/cornell/library/integration/voyager/Locations.java
+++ src/main/java/edu/cornell/library/integration/voyager/Locations.java
@@ -11,17 +11,32 @@ import java.util.Map;
 import edu.cornell.library.integration.ilcommons.configuration.SolrBuildConfig;
 import edu.cornell.library.integration.utilities.IndexingUtilities;
 
+/**
+ * Give access to location data by code or number. Will load loaded into memory from the Voyager
+ * database on the first instantiation of the class.
+ */
 public final class Locations {
-	SolrBuildConfig config;
-	private static final Map<String,Location> _byCode = new HashMap<>();
-	private static final Map<Integer,Location> _byNumber = new HashMap<>();
 
+	/**
+	 * Retrieve Location object based on <b>code</b>. The value will already have been loaded
+	 * into memory. The method cannot be called statically to ensure that the instantiation has
+	 * been able to load the data.
+	 * @param code
+	 * @return Location
+	 */
 	@SuppressWarnings("static-method")
 	public final Location getByCode( final String code ) {
 		if (_byCode.containsKey(code))
 			return _byCode.get(code);
 		return null;
 	}
+	/**
+	 * Retrieve Location object based on <b>number</b>. The value will already have been loaded
+	 * into memory. The method cannot be called statically to ensure that the instantiation has
+	 * been able to load the data.
+	 * @param number
+	 * @return Location
+	 */
 	@SuppressWarnings("static-method")
 	public final Location getByNumber( final int number ) {
 		if (_byNumber.containsKey(number))
@@ -29,6 +44,16 @@ public final class Locations {
 		return null;
 	}
 
+	/**
+	 * Structure containing values relating to holdings location. <b>Name</b> and <b>library</b>
+	 * may potentially be null.<br/>
+	 * <dl>
+	 *  <dt>code</dt><dd>Location code, as appears in holdings 852$b, e.g. "fine,res"</dd>
+	 *  <dt>number</dt><dd>Location number, as appears in item record, e.g. 33</dd>
+	 *  <dt>name</dt><dd>Location name, e.g. "Fine Arts Library Reserve"</dd>
+	 *  <dt>library</dt><dd>Unit Library name, e.g. "Fine Arts Library"</dd>
+	 * </dl>
+	 */
 	public static class Location implements Comparable<Location>{
 		public final String code;
 		public final Integer number;
@@ -51,20 +76,30 @@ public final class Locations {
 		public int compareTo( final Location other ) {
 			return this.number.compareTo(other.number);
 		}
-		public Location (String code, Integer number, String name, String library) {
+		private Location (String code, Integer number, String name, String library) {
 			this.code = code;
 			this.number = number;
 			this.name = name;
 			this.library = library;
 		}
 	}
+
+	/**
+	 * Give access to location data by code or number. If this is the first time Locations has
+	 * been instantiated in the current process, instantiation will attempt to connect to Voyager
+	 * to retrieve and index the data. Otherwise, the instance will simply give access to the
+	 * already loaded data.
+	 */
 	public Locations(final SolrBuildConfig config) throws ClassNotFoundException, SQLException {
-		this.config = config;
+
 		if (_byCode.isEmpty())
-			populateLocationMaps();
+			populateLocationMaps(config);
 	}
 
-// PRIVATE METHODS
+// PRIVATE RESOURCES
+
+	private static final Map<String,Location> _byCode = new HashMap<>();
+	private static final Map<Integer,Location> _byNumber = new HashMap<>();
 
 	private static final String getLocationsQuery =
 			"SELECT "
@@ -73,8 +108,12 @@ public final class Locations {
 			+ "LOCATION.LOCATION_DISPLAY_NAME, "
 			+ "LOCATION.LOCATION_NAME "
 			+"FROM LOCATION ";
-	private void populateLocationMaps() throws ClassNotFoundException, SQLException {
+
+	private static void populateLocationMaps(final SolrBuildConfig config)
+			throws ClassNotFoundException, SQLException {
 		System.out.println("Retrieving location data from Voyager");
+		libraryPatterns = IndexingUtilities.loadPatternMap("library_names.txt");
+
 		try ( Connection voyager = config.getDatabaseConnection("Voy");
 				Statement stmt = voyager.createStatement();
 				ResultSet rs = stmt.executeQuery(getLocationsQuery)) {
@@ -85,18 +124,14 @@ public final class Locations {
 				if (name == null)
 					name = rs.getString(4);
 				Location l = new Location(rs.getString(1),rs.getInt(2),name,getLibrary(name));
-
 				_byCode.put(l.code, l);
 				_byNumber.put(l.number, l);
 			}
-			
 		}
 	}
 	private static String getLibrary(String name) {
 		if (name == null)
 			return null;
-		if (libraryPatterns == null)
-			libraryPatterns = IndexingUtilities.loadPatternMap("library_names.txt");
 		Iterator<String> i = libraryPatterns.keySet().iterator();
 		while (i.hasNext()) {
 			String pattern = i.next();
