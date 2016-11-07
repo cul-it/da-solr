96de4f7f93e173f89c0b33cbc9d4a36146e0fb53
Frances Webb <frances.webb@cornell.edu>
2016-11-07 12:13:02 -0500

    Lock queue table under alias as well as directly
    The query that uses the alias is cleaner with the alias, so I'd rather
    not rewrite it. It doesn't need write access though, so locking with
    read access only should do.

--- src/main/java/edu/cornell/library/integration/indexer/BatchRecordsForSolrIndex.java
+++ src/main/java/edu/cornell/library/integration/indexer/BatchRecordsForSolrIndex.java
@@ -35,7 +35,7 @@ public class BatchRecordsForSolrIndex {
         Set<Integer> addedBibs = new HashSet<>(minCount);
 
         try (Statement stmt = current.createStatement()) {
-        	stmt.executeQuery("LOCK TABLES "+CurrentDBTable.QUEUE+" WRITE"); }
+        	stmt.executeQuery("LOCK TABLES "+CurrentDBTable.QUEUE+" WRITE, "+CurrentDBTable.QUEUE+" AS q READ"); }
         try (PreparedStatement pstmt = current.prepareStatement(
         		" SELECT q.bib_id, cause"
         		+"  FROM "+CurrentDBTable.QUEUE+" AS q"
