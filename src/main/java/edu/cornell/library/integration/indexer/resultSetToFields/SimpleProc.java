42040e73b79d67713e1b1a0f307af4470c7bcc4f
Frances Webb <frances.webb@cornell.edu>
2017-06-08 16:56:23 -0400

    Correct query name referenced in SimpleProc.java

--- src/main/java/edu/cornell/library/integration/indexer/resultSetToFields/SimpleProc.java
+++ src/main/java/edu/cornell/library/integration/indexer/resultSetToFields/SimpleProc.java
@@ -21,7 +21,7 @@ public class SimpleProc implements ResultSetToFields {
 			Map<String, ResultSet> results, SolrBuildConfig config) throws Exception {
 
 		MarcRecord rec = new MarcRecord(MarcRecord.RecordType.BIBLIOGRAPHIC);
-		rec.addDataFieldResultSet(results.get("instrumentation"));
+		rec.addDataFieldResultSet(results.get("notes"));
 
 		Map<String,SolrInputField> fields = new HashMap<>();
 		SolrFields vals = generateSolrFields( rec, null );
