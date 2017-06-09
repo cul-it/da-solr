7917acd716a8631fe5c07ffee4f7fe285b232843
Frances Webb <frances.webb@cornell.edu>
2017-06-09 11:02:08 -0400

    Modernize Instrumentation.java; Attach unit tests

--- src/main/java/edu/cornell/library/integration/indexer/resultSetToFields/Instrumentation.java
+++ src/main/java/edu/cornell/library/integration/indexer/resultSetToFields/Instrumentation.java
@@ -1,20 +1,16 @@
 package edu.cornell.library.integration.indexer.resultSetToFields;
 
-import static edu.cornell.library.integration.indexer.resultSetToFields.ResultSetUtilities.addField;
-
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.solr.common.SolrInputField;
 
 import com.hp.hpl.jena.query.ResultSet;
 
 import edu.cornell.library.integration.ilcommons.configuration.SolrBuildConfig;
+import edu.cornell.library.integration.indexer.resultSetToFields.ResultSetUtilities.SolrField;
+import edu.cornell.library.integration.indexer.resultSetToFields.ResultSetUtilities.SolrFields;
 import edu.cornell.library.integration.marc.DataField;
-import edu.cornell.library.integration.marc.DataFieldSet;
 import edu.cornell.library.integration.marc.MarcRecord;
 import edu.cornell.library.integration.marc.Subfield;
 
@@ -30,82 +26,86 @@ public class Instrumentation implements ResultSetToFields {
 	public Map<String, SolrInputField> toFields(
 			Map<String, ResultSet> results, SolrBuildConfig config) throws Exception {
 
-		Collection<DataFieldSet> sets = ResultSetUtilities.resultSetsToSetsofMarcFields(
-				MarcRecord.RecordType.BIBLIOGRAPHIC,results);
+		MarcRecord rec = new MarcRecord(MarcRecord.RecordType.BIBLIOGRAPHIC);
+		rec.addDataFieldResultSet(results.get("instrumentation"));
 
 		Map<String,SolrInputField> fields = new HashMap<>();
-		for( DataFieldSet fs: sets ) {
+		SolrFields vals = generateSolrFields( rec, null );
 
-			Set<String> values880 = new HashSet<>();
-			Set<String> valuesMain = new HashSet<>();
-
-			for (DataField f: fs.getFields()) {
-				String total_performers = null;
-				StringBuilder sb = new StringBuilder();
-				boolean forAppended = false;
-				for ( Subfield sf : f.subfields ) {
-					switch (sf.code) {
-					case 'a':
-						if (forAppended) sb.append("; ");
-						else{ sb.append(FOR); forAppended = true; }
-						sb.append(sf.value);
-						break;
-					case 'b':
-						if (forAppended) sb.append("; ");
-						else{ sb.append(FOR); forAppended = true; }
-						sb.append("solo ").append(sf.value);
-						break;
-					case 'd':
-						if (forAppended) sb.append('/');
-						else{ sb.append(FOR); forAppended = true; }
-						sb.append(sf.value);
-						break;
-					case 'n':
-					case 'e':
-						try {
-							int count = Integer.valueOf(sf.value);
-							if (count > 1) {
-								if (forAppended) sb.append(' ');
-								else{ sb.append(FOR); forAppended = true; }
-								sb.append('(').append(count).append(')');
-							}
-						} catch (@SuppressWarnings("unused") NumberFormatException e) {
-							System.out.println("382$n/e is not an integer ("+sf.value+").");
-						}
-						break;
-					case 'p':
-						if (forAppended) sb.append(" or ");
-						else{ sb.append(FOR); forAppended = true; }
-						sb.append(sf.value);
-						break;
-					case 's':
-						total_performers = sf.value;
-						break;
-					case '3':
-					case 'v':
-						if (forAppended)
-							sb.append(" [").append(sf.value).append(']');
-						else
-							sb.append('[').append(sf.value).append("]: ");
-						break;
-					}
-				}
-				if (total_performers != null) {
-					if (sb.length() > 0) sb.append(". ");
-					sb.append("Total performers: ").append(total_performers);
-				}
-				if (f.tag.equals("880"))
-					values880.add(sb.toString());
-				else
-					valuesMain.add(sb.toString());
-			}
-			for ( String s : values880 )
-				addField(fields,"instrumentation_display",s);
-			for ( String s : valuesMain )
-				addField(fields,"instrumentation_display",s);
-		}
+		for ( SolrField f : vals.fields )
+			ResultSetUtilities.addField(fields, f.fieldName, f.fieldValue);
 
 		return fields;
+	}
+
+	/**
+	 * @param config Is unused, but included to follow a consistent method signature. 
+	 */
+	public static SolrFields generateSolrFields( MarcRecord rec, SolrBuildConfig config ) {
+
+		SolrFields sfs = new SolrFields();
+		for( DataField f: rec.matchSortAndFlattenDataFields() ) {
+
+			String total_performers = null;
+			StringBuilder sb = new StringBuilder();
+			boolean forAppended = false;
+			for ( Subfield sf : f.subfields ) {
+				switch (sf.code) {
+				case 'a':
+					if (forAppended) sb.append("; ");
+					else{ sb.append(FOR); forAppended = true; }
+					sb.append(sf.value);
+					break;
+				case 'b':
+					if (forAppended) sb.append("; ");
+					else{ sb.append(FOR); forAppended = true; }
+					sb.append("solo ").append(sf.value);
+					break;
+				case 'd':
+					if (forAppended) sb.append('/');
+					else{ sb.append(FOR); forAppended = true; }
+					sb.append(sf.value);
+					break;
+				case 'n':
+				case 'e':
+					try {
+						int count = Integer.valueOf(sf.value);
+						if (count > 1) {
+							if (forAppended) sb.append(' ');
+							else{ sb.append(FOR); forAppended = true; }
+							sb.append('(').append(count).append(')');
+						}
+					} catch (@SuppressWarnings("unused") NumberFormatException e) {
+						System.out.println("382$n/e is not an integer ("+sf.value+").");
+					}
+					break;
+				case 'p':
+					if (forAppended) sb.append(" or ");
+					else{ sb.append(FOR); forAppended = true; }
+					sb.append(sf.value);
+					break;
+				case 's':
+					total_performers = sf.value;
+					break;
+				case '3':
+				case 'v':
+					if (forAppended)
+						sb.append(" [").append(sf.value).append(']');
+					else
+						sb.append('[').append(sf.value).append("]: ");
+					break;
+				}
+			}
+			if (total_performers != null) {
+				if (sb.length() > 0) sb.append(". ");
+				sb.append("Total performers: ").append(total_performers);
+			}
+
+			String instrumentation = sb.toString();
+			sfs.add(new SolrField("instrumentation_display",instrumentation));
+			sfs.add(new SolrField("notes_t",instrumentation));
+		}
+		return sfs;
 	}
 
 	private static final String FOR = "For ";
