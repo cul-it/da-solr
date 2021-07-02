b82cdb8a67927fccc0b8cfeb7f20ed6a51834d37
Frances Webb <frances.webb@cornell.edu>
2021-07-02 08:37:18 -0400

    Try an upgrade to Jackson version

--- src/main/java/edu/cornell/library/integration/folio/OkapiClient.java
+++ src/main/java/edu/cornell/library/integration/folio/OkapiClient.java
@@ -2,7 +2,6 @@ package edu.cornell.library.integration.folio;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -169,7 +168,6 @@ public class OkapiClient {
 			while ((line = br.readLine()) != null) {
 				response.append(line + "\n");
 			}
-			System.out.println(response.toString());
 			return response.toString();
 		}
 	}
