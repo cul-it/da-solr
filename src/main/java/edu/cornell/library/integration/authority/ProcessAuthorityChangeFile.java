8f6541f145e4c8b0997d9c81768146cc58661420
Frances Webb <frances.webb@cornell.edu>
2023-10-19 09:57:03 -0400

    Make just one db connection
    The db connection pooler is set to one connection, and while that could be changed, we don't really need multiple connections here.
    DISCOVERYACCESS-7439
    FOL-183

--- src/main/java/edu/cornell/library/integration/authority/ProcessAuthorityChangeFile.java
+++ src/main/java/edu/cornell/library/integration/authority/ProcessAuthorityChangeFile.java
@@ -247,7 +247,8 @@ public class ProcessAuthorityChangeFile {
 				jsonWriter.close();
 
 				uploadFileToBox(env.get("boxKeyFile"),"JSON folder",outputFile);
-				registerReportCompletion(config,firstFile,lastFile, requesterName, requesterEmail,outputFile);
+				registerReportCompletion(authority,config.getServerConfig("authReportEmail"),
+						firstFile,lastFile, requesterName, requesterEmail,outputFile);
 				System.out.println(count);
 				System.out.println("Entailed bibs:");
 				for (String entailedBib : entailedBibs)
@@ -257,26 +258,24 @@ public class ProcessAuthorityChangeFile {
 	}
 
 	private static void registerReportCompletion(
-			Config config, String firstFile, String lastFile, String toName, String toEmail, String file)
+			Connection db, Map<String,String> authReportEmailConfig, String firstFile, String lastFile, String toName, String toEmail, String file)
 			throws SQLException {
-		try (Connection authdb = config.getDatabaseConnection("Authority");
-				PreparedStatement stmt = authdb.prepareStatement(
+		try (PreparedStatement stmt = db.prepareStatement(
 						"UPDATE lcAuthorityFile SET reportedDate = NOW() WHERE updateFile BETWEEN ? AND ?")){
 			stmt.setString(1, firstFile);
 			stmt.setString(2, lastFile);
 			stmt.executeUpdate();
-			Map<String,String> authReportEmailConfig = config.getServerConfig("authReportEmail");
-			System.out.printf("Subject: %s: %s\n",authReportEmailConfig.get("Subject"), file);
-			System.out.printf("From: %s\n",authReportEmailConfig.get("From"));
-			System.out.printf("To: %s <%s>\n",toName, toEmail);
-			System.out.println();
-			try (PreparedStatement s = authdb.prepareStatement(
-					"select * from lcAuthorityFile where reportedDate is null order by updateFile");
-					ResultSet rs = s.executeQuery()) {
-				System.out.printf("| %10s | %8s | %8s |\n","FILE","POSTED DATE", "IMPORT DATE");
-				while (rs.next())
-					System.out.printf("| %10s | %11s | %11s |\n", rs.getString(1),rs.getTimestamp(2),rs.getTimestamp(3));
-			}
+		}
+		System.out.printf("Subject: %s: %s\n",authReportEmailConfig.get("Subject"), file);
+		System.out.printf("From: %s\n",authReportEmailConfig.get("From"));
+		System.out.printf("To: %s <%s>\n",toName, toEmail);
+		System.out.println();
+		try (PreparedStatement s = db.prepareStatement(
+				"select * from lcAuthorityFile where reportedDate is null order by updateFile");
+				ResultSet rs = s.executeQuery()) {
+			System.out.printf("| %10s | %8s | %8s |\n","FILE","POSTED DATE", "IMPORT DATE");
+			while (rs.next())
+				System.out.printf("| %10s | %11s | %11s |\n", rs.getString(1),rs.getTimestamp(2),rs.getTimestamp(3));
 		}
 	}
 
