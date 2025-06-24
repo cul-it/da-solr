ecffd12001007c6596523cf18d1fbece9299a742
Frances Webb <frances.webb@cornell.edu>
2025-06-24 10:37:28 -0400

    Adjust filter flags for hathi links to differentiate from regular links
    The filter flags url_access and url_other were intended to differentiate between links created from Folio links from supplementary HathiTrust links added based on hathifiles data. But because the HathiLinks.java logic uses the URL.java work, the flags were populated in both places.
    
    Additionally, the test suite needed to be updated with the change. I also uncommented a test for an in-copyright link, and one for a link pulled in using an OCLC number, as both major cases were missing.
    
    DACCESS-591

--- src/test/java/edu/cornell/library/integration/metadata/generator/HathiLinksTest.java
+++ src/test/java/edu/cornell/library/integration/metadata/generator/HathiLinksTest.java
@@ -39,6 +39,7 @@ public class HathiLinksTest extends DbBaseTest {
 		"notes_t: HathiTrust (multiple volumes)\n"+
 		"url_access_json: {\"description\":\"HathiTrust (multiple volumes)\",\"url\":"
 		+ "\"http://catalog.hathitrust.org/Record/008595162\"}\n"+
+		"availability_facet: url_access_hathi\n"+
 		"online: Online\n"+
 		"hathi_title_data: 008595162\n";
 		assertEquals( expected, this.gen.generateSolrFields(rec, config).toString() );
@@ -51,21 +52,26 @@ public class HathiLinksTest extends DbBaseTest {
 		String expected =
 		"notes_t: HathiTrust\n"+
 		"url_access_json: {\"description\":\"HathiTrust\",\"url\":\"http://hdl.handle.net/2027/coo.31924005214295\"}\n"+
+		"availability_facet: url_access_hathi\n"+
 		"online: Online\n"+
 		"hathi_title_data: 100174680\n";
 		assertEquals( expected, this.gen.generateSolrFields(rec, config).toString() );
 	}
-/*
+
 	@Test
 	public void testRestrictedLink() throws SQLException, IOException {
 		MarcRecord rec = new MarcRecord( MarcRecord.RecordType.BIBLIOGRAPHIC );
 		rec.id = "4";
+		// The OCLC number matches the value in HT, but should not matter as we don't include deny
+		// links for OCLC matches.
+		rec.dataFields.add(new DataField(1,"035",' ',' ',"‡a (OCoLC)63226685"));
 		String expected =
-		"url_other_display: http://catalog.hathitrust.org/Record/009226070"
+		"url_other_display: http://catalog.hathitrust.org/Record/009226070" 
 		+ "|HathiTrust – Access limited to full-text search\n"+
 		"notes_t: HathiTrust – Access limited to full-text search\n"+
-		"hathi_title_data: 009226070\n"+
-		"notes_t: Connect to full text. Access limited to authorized subscribers.\n" + 
+		"availability_facet: url_other_hathi\n"+
+		"hathi_title_data: 009226070\n";
+/*		"notes_t: Connect to full text. Access limited to authorized subscribers.\n" + 
 		"url_access_json: {\"description\":\"Connect to full text. Access limited to authorized subscribers.\","
 		+ "\"url\":\"https://hdl.handle.net/2027/coo.31924003850009?urlappend=%3B"
 		+ "signon=swle:https://shibidp.cit.cornell.edu/idp/shibboleth\"}\n" + 
@@ -74,10 +80,27 @@ public class HathiLinksTest extends DbBaseTest {
 		"url_access_json: {\"description\":\"Information for users about temporary access\","
 		+ "\"url\":\"https://www.hathitrust.org/ETAS-User-Information\"}\n" + 
 		"online: Online\n" + 
-		"etas_facet: 1\n";
+		"etas_facet: 1\n";*/
 		assertEquals( expected, this.gen.generateSolrFields(rec, config).toString() );
 	}
 
+	@Test
+	public void testOCLCLink() throws SQLException, IOException {
+		MarcRecord rec = new MarcRecord( MarcRecord.RecordType.BIBLIOGRAPHIC );
+		rec.id = "16600340";
+		rec.dataFields.add(new DataField(1,"035",' ',' ',"‡a (OCoLC)1456587331"));
+		rec.dataFields.add(new DataField(2,"035",' ',' ',"‡a (OCoLC)45072"));
+		String expected =
+		"notes_t: HathiTrust\n"+
+		"url_access_json: {\"description\":\"HathiTrust\",\"url\":\"http://hdl.handle.net/2027/mdp.39015015428694\"}\n"+
+		"availability_facet: url_access_hathi\n"+
+		"online: Online\n"+
+		"hathi_title_data: 001475006\n";
+		assertEquals( expected, this.gen.generateSolrFields(rec, config).toString() );
+	}
+
+
+/*
 	@Test
 	public void blockEtasLinkWhenNobody() throws SQLException, IOException {
 		MarcRecord rec = new MarcRecord( MarcRecord.RecordType.BIBLIOGRAPHIC );
@@ -101,6 +124,7 @@ public class HathiLinksTest extends DbBaseTest {
 		"notes_t: HathiTrust\n"+
 		"url_access_json: {\"description\":\"HathiTrust\","
 		+ "\"url\":\"http://hdl.handle.net/2027/coo1.ark:/13960/t20c5hb54\"}\n"+
+		"availability_facet: url_access_hathi\n"+
 		"online: Online\n"+
 		"hathi_title_data: 100763896\n";
 		assertEquals( expected, this.gen.generateSolrFields(rec, config).toString() );
@@ -133,13 +157,7 @@ public class HathiLinksTest extends DbBaseTest {
 		assertEquals( "", this.gen.generateSolrFields(rec, config).toString() );
 	}
 
-	@Test
-	public void oddbehavior() throws SQLException, IOException {
-		MarcRecord rec = new MarcRecord( MarcRecord.RecordType.BIBLIOGRAPHIC );
-		rec.dataFields.add(new DataField(1,"035",' ',' ',"‡a (OCoLC)61353090"));
-		System.out.println(this.gen.generateSolrFields(rec, config).toString());
-	}
-	
+
 	@Test
 	public void testMultipleSourceInstRecNum() throws SQLException, IOException {
 		MarcRecord rec = new MarcRecord( MarcRecord.RecordType.BIBLIOGRAPHIC );
@@ -148,6 +166,7 @@ public class HathiLinksTest extends DbBaseTest {
 		"notes_t: HathiTrust\n"+
 		"url_access_json: {\"description\":\"HathiTrust\","
 		+ "\"url\":\"http://hdl.handle.net/2027/coo.31924000030001\"}\n"+
+		"availability_facet: url_access_hathi\n"+
 		"online: Online\n"+
 		"hathi_title_data: 102756782\n";
 		assertEquals( expected, this.gen.generateSolrFields(rec, config).toString() );
