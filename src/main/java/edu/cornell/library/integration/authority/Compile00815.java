2bbb3a7953c77342544b43ca75490b292e088792
Frances Webb <frances.webb@cornell.edu>
2026-02-20 08:32:12 -0500

    Make report json, add links
    FOL-287

--- src/main/java/edu/cornell/library/integration/authority/Compile00815.java
+++ src/main/java/edu/cornell/library/integration/authority/Compile00815.java
@@ -2,21 +2,32 @@ package edu.cornell.library.integration.authority;
 
 
 import static edu.cornell.library.integration.authority.IndexAuthorityRecords.getAllIdentifiers;
-import static edu.cornell.library.integration.authority.Solr.querySolrForMatchingBibCount;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import javax.xml.stream.XMLStreamException;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.Http2SolrClient;
+import org.apache.solr.common.SolrDocumentList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import edu.cornell.library.integration.marc.ControlField;
 import edu.cornell.library.integration.marc.DataField;
@@ -30,15 +41,23 @@ public class Compile00815 {
 			throws FileNotFoundException, IOException, SQLException {
 
 		Collection<String> requiredArgs =  Config.getRequiredArgsForDB("Authority");
+		requiredArgs.add("blacklightUrl");
+		requiredArgs.add("blacklightSolrUrl");
+
 		Config config = Config.loadConfig(requiredArgs);
-		System.out.println("identifier\trecord source\tMARC field\theading\tbib count");
+
+		String outputFile = "report-"+Math.abs(UUID.randomUUID().toString().hashCode())+".json";
+		String blacklightUrl = config.getBlacklightUrl();
 
 		try ( Connection authority = config.getDatabaseConnection("Authority") ;
 				Http2SolrClient solr = new Http2SolrClient
 						.Builder(config.getBlacklightSolrUrl())
 						.withBasicAuthCredentials(config.getSolrUser(),config.getSolrPassword()).build();
+				BufferedWriter jsonWriter = Files.newBufferedWriter(Paths.get(outputFile));
 				){
 			Set<String> identifiers = getAllIdentifiers(authority);
+			jsonWriter.append("[\n");
+			boolean writtenJson = false;
 
 			for (String identifier : identifiers) {
 				String heading = null;
@@ -91,33 +110,38 @@ public class Compile00815 {
 				if ( mainTag == null || ! mainTag.equals("151")) continue;
 
 				// Look for bibs matching main heading
-				int headingMatches = querySolrForMatchingBibCount(solr, "subject_geo_browse", heading, false);
-				if (headingMatches > 0)
-					System.out.format("%s\t%s\t151\t%s\t%d\n", identifier, recSource, heading, headingMatches);
-
-
-				String geographicSubdivision = get781Subdivision(rec);
-				if (geographicSubdivision != null) {
-
-					headingMatches = querySolrForMatchinSubdivisionBibCount(solr, geographicSubdivision);
-					if (headingMatches > 0)
-						System.out.format("%s\t%s\t781\t%s\t%d\n", identifier, recSource, geographicSubdivision, headingMatches);
+				Map<String,Object> mhb = querySolrForMatchingBibCount(
+						solr, "subject_geo_browse", heading, "151", blacklightUrl);
+				if (mhb != null) {
+					mhb.put("identifier", identifier); mhb.put("record source", recSource);
+					if ( writtenJson ) jsonWriter.append(",\n"); else writtenJson = true;
+					jsonWriter.append(mapper.writeValueAsString(mhb));
+					jsonWriter.flush();
 				}
 
+				// Potentially look for bibs matching secondary subdivision heading
+				String geographicSubdivision = get781Subdivision(rec);
+				if (geographicSubdivision != null)
+					for (HeadingVocab vocab : HeadingVocab.values()) {
+						String solrField = "subject_sub_"+vocab.name().toLowerCase()+"_browse";
+						Map<String,Object> sdb = querySolrForMatchingBibCount(
+								solr, solrField, geographicSubdivision, "781", blacklightUrl);
+						if (sdb != null) {
+							sdb.put("identifier", identifier); sdb.put("record source", recSource);
+							if ( writtenJson ) jsonWriter.append(",\n"); else writtenJson = true;
+							jsonWriter.append(mapper.writeValueAsString(sdb));
+						}
+
+					}
+
 			}
+			jsonWriter.append("]\n");
+			jsonWriter.flush();
+			jsonWriter.close();
 
 		} catch (SolrServerException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private static int querySolrForMatchinSubdivisionBibCount(Http2SolrClient solr, String heading) throws SolrServerException, IOException {
-		int count = 0;
-		for (HeadingVocab vocab : HeadingVocab.values()) {
-			String solrField = "subject_sub_"+vocab.name().toLowerCase()+"_browse";
-			count += querySolrForMatchingBibCount(solr, solrField, heading, false);
-		}
-		return count;
 	}
 
 	private static String get781Subdivision(MarcRecord rec) {
@@ -137,5 +161,38 @@ public class Compile00815 {
 		}
 		return null;
 	}
+
+
+	public static Map<String,Object> querySolrForMatchingBibCount(
+			Http2SolrClient solr,
+			String field,
+			String heading,
+			String marcTag,
+			String blacklightUrl
+			)  throws SolrServerException, IOException {
+
+		String query = field+":\""+heading.replaceAll("\"","'").replaceAll("\\\\","")+'"';
+		SolrQuery q = new SolrQuery(query);
+		q.setRows(0);
+		q.setFields("instance_id","id");
+		SolrDocumentList res = solr.query(q).getResults();
+		long found = res.getNumFound();
+		if (found == 0) return null;
+
+		// build output block
+		Map<String,Object> b = new HashMap<>();
+		b.put("marc tag", marcTag);
+		b.put("heading", heading);
+		b.put("solr link",
+				solr.getBaseURL()+"/select?qt=search&wt=csv&rows=99999&fl=instance_id&q="+query.replaceAll("\"", "%22"));
+		b.put("bib count", found);
+		if (marcTag.equals("151"))
+			b.put("blacklight link",
+					blacklightUrl+"/?q=%22"+URLEncoder.encode(heading, "UTF-8")+"%22&search_field="+field);
+		return b;
+	}
+
+	static ObjectMapper mapper = new ObjectMapper();
+	static { mapper.enable(SerializationFeature.INDENT_OUTPUT); }
 
 }
